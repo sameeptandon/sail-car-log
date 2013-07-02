9baa5bf42ddb2222224e3db596176b9d018536d3
SAIL Laptop <no@email.com>
2013-07-02 12:15:53 -0700

    current version broken

--- process/ProjectGPSonVideo.py
+++ process/ProjectGPSonVideo.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
   video_filename = sys.argv[1]
   gps_filename = sys.argv[2]
 
-  num_imgs_fwd = 200; 
+  num_imgs_fwd = 500; 
 
   video_reader = VideoReader(video_filename)
   gps_reader = GPSReader(gps_filename)
@@ -19,13 +19,17 @@ if __name__ == '__main__':
   R_to_c_from_i = array([[-1, 0, 0], \
                          [0, 0, -1], \
                          [0, -1, 0]]);
-  R_camera_pitch = euler_matrix(-deg2rad(2.0), deg2rad(5.0), deg2rad(0.0), 'sxyz')[0:3,0:3]
+  R_camera_pitch = euler_matrix(-deg2rad(0.0), deg2rad(0), deg2rad(0.0), 'sxyz')[0:3,0:3]
   R_to_c_from_i = dot(R_camera_pitch, R_to_c_from_i) 
 
-  cam = { } 
-  cam['f'] = 2271.3;
-  cam['cu'] = 622.0338;
-  cam['cv'] = 419.4885;
+  cam = { }
+  cam['fx'] = 2221.8
+  cam['fy'] = 2233.7
+  cam['cu'] = 623.7
+  cam['cv'] = 445.7
+  #cam['f'] = 2271.3;
+  #cam['cu'] = 622.0338;
+  #cam['cv'] = 419.4885;
   #cam['f'] = 2250.0;
   #cam['cu'] = 640.0;
   #cam['cv'] = 480.0;
@@ -39,26 +43,36 @@ if __name__ == '__main__':
     (success, I) = video_reader.getNextFrame()
     if success == False:
       break
-    if framenum % 10 != 0:
+    if framenum % 5 != 0:
       continue
 
-    roll_start = deg2rad(gps_dat[framenum,7]);
-    pitch_start = -deg2rad(gps_dat[framenum,8]);
-    yaw_start = deg2rad(gps_dat[framenum,9]);
+    #roll_start = deg2rad(gps_dat[framenum,7]);
+    #pitch_start = -deg2rad(gps_dat[framenum,8]);
+    #yaw_start = deg2rad(gps_dat[framenum,9]);
+
+    pitch_start = deg2rad(gps_dat[framenum,7]);
+    roll_start = -deg2rad(gps_dat[framenum,8]);
+    yaw_start = deg2rad(gps_dat[framenum,9] + 90);
 
     R_to_i_from_w = \
     euler_matrix(pitch_start,roll_start,yaw_start,'rxyz')[0:3,0:3]
 
-    pts = zeros([num_imgs_fwd, 3])
-    for t in range(num_imgs_fwd):
+    # pts = zeros([num_imgs_fwd, 3])
+    # for t in range(num_imgs_fwd):
+    pts = zeros([min(gps_dat.shape[0]-framenum,1000),3])
+    for t in range(pts.shape[0]):
       pts[t,:] = WGS84toENU(gps_dat[framenum, 1:4], gps_dat[framenum+t, 1:4])
-      pts[t,2] = pts[t,2] - 1.1 
+      pts[t,2] = pts[t,2] 
 
     for idx in range(1,pts.shape[0]):
       world_coordinates = pts[idx,:]
       pos_wrt_imu = dot(R_to_i_from_w, world_coordinates)
-      pos_wrt_camera = dot(R_to_c_from_i, (pos_wrt_imu + array([-0.25, -2.13,
-        0])))
+      pos_wrt_camera = dot(R_to_c_from_i, (pos_wrt_imu + array([0.25, -1.1,
+        -0.2])))
+      #pos_wrt_camera = dot(R_to_c_from_i, (pos_wrt_imu + array([-0.25, -2.13,
+      #  0])))
+      # quick hack added by Tao
+      #pos_wrt_camera[2] = pos_wrt_camera[2]*1.4
       pix = around(dot(cam['KK'], divide(pos_wrt_camera, pos_wrt_camera[2])))
       if (pix[0] > 0 and pix[0] < 1280 and pix[1] > 0 and pix[1] < 960):
         I[pix[1]-3:pix[1]+3, pix[0]-3:pix[0]+3, 0] = 255;
