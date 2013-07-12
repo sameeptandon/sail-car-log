c3f68b7567960494b6e37536ba0309ea140be2d2
SAIL Laptop <no@email.com>
2013-07-12 12:25:19 -0700

    refactor

---
+++ process/GPSReprojection.py
@@ -0,0 +1,51 @@
+from transformations import euler_matrix
+import numpy as np
+from WGS84toENU import *
+from numpy import array, dot, zeros, around, divide, ones
+
+def GPSMask(GPSData, Camera): 
+
+    roll_start = -deg2rad(GPSData[0,7]);
+    pitch_start = deg2rad(GPSData[0,8]);
+    yaw_start = -deg2rad(GPSData[0,9]+90);
+
+    psi = pitch_start; 
+    cp = cos(psi);
+    sp = sin(psi);
+    theta = roll_start;
+    ct = cos(theta);
+    st = sin(theta);
+    gamma = yaw_start;
+    cg = cos(gamma);
+    sg = sin(gamma);
+
+    R_to_i_from_w = \
+            array([[cg*cp-sg*st*sp, -sg*ct, cg*sp+sg*st*cp],
+                  [sg*cp+cg*st*sp, cg*ct, sg*sp-cg*st*cp],
+                  [-ct*sp, st, ct*cp]]).transpose()
+
+    pts = zeros([GPSData.shape[0],3])
+    for t in range(pts.shape[0]):
+      pts[t,:] = WGS84toENU(GPSData[0, 1:4], GPSData[t, 1:4])
+
+    R_to_c_from_i = Camera['R_to_c_from_i']
+    R_camera_pitch = euler_matrix(Camera['rot_x'], Camera['rot_y'],\
+            Camera['rot_z'], 'sxyz')[0:3,0:3]
+    R_to_c_from_i = dot(R_camera_pitch, R_to_c_from_i) 
+
+    #I = zeros((960,1280,3), np.uint8)
+    I = 255*ones((960,1280,3), np.uint8)
+
+    for idx in range(1,pts.shape[0]):
+      world_coordinates = pts[idx,:]
+      pos_wrt_imu = dot(R_to_i_from_w, world_coordinates)
+      pos_wrt_camera = dot(R_to_c_from_i, (pos_wrt_imu + array([0, 0.0, -0])))
+      pos_wrt_camera[1] = pos_wrt_camera[1] + 1.1; # move to ground
+      pix = around(dot(Camera['KK'], divide(pos_wrt_camera, pos_wrt_camera[2])))
+      if (pix[0] > 0 and pix[0] < 1280 and pix[1] > 0 and pix[1] < 960):
+        I[pix[1]-2:pix[1]+2, pix[0]-2:pix[0]+2, 0] = 0;
+        I[pix[1]-2:pix[1]+2, pix[0]-2:pix[0]+2, 1] = 0;
+        I[pix[1]-2:pix[1]+2, pix[0]-2:pix[0]+2, 2] = 0;
+    
+    return I
+
--- process/ProjectGPSonVideo.py
+++ process/ProjectGPSonVideo.py
@@ -2,37 +2,31 @@ import sys
 from GPSReader import *
 from VideoReader import *
 from WGS84toENU import *
+from GPSReprojection import *
 from transformations import euler_matrix
 from numpy import array, dot, zeros, around, divide
-from cv2 import imshow, waitKey
+from cv2 import imshow, waitKey, resize
 
 if __name__ == '__main__':
   video_filename = sys.argv[1]
   gps_filename = sys.argv[2]
-
-  num_imgs_fwd = 500; 
-
+  num_imgs_fwd = 125; 
   video_reader = VideoReader(video_filename)
   gps_reader = GPSReader(gps_filename)
   gps_dat = gps_reader.getNumericData()
-  
-  R_to_c_from_i = array([[-1, 0, 0], \
-                         [0, 0, -1], \
-                         [0, -1, 0]]);
-  R_camera_pitch = euler_matrix(deg2rad(-0.7), deg2rad(0.3), deg2rad(0.0), 'sxyz')[0:3,0:3]
-  R_to_c_from_i = dot(R_camera_pitch, R_to_c_from_i) 
 
   cam = { }
+  cam['R_to_c_from_i'] = array([[-1, 0, 0], \
+                         [0, 0, -1], \
+                         [0, -1, 0]]);
+  cam['rot_x'] = deg2rad(-0.7);
+  cam['rot_y'] = deg2rad(0.3);
+  cam['rot_z'] = deg2rad(0.0);
+
   cam['fx'] = 2221.8
   cam['fy'] = 2233.7
   cam['cu'] = 623.7
   cam['cv'] = 445.7
-  #cam['f'] = 2271.3;
-  #cam['cu'] = 622.0338;
-  #cam['cv'] = 419.4885;
-  #cam['f'] = 2250.0;
-  #cam['cu'] = 640.0;
-  #cam['cv'] = 480.0;
   cam['KK'] = array([[cam['fx'], 0.0, cam['cu']], \
                      [0.0, cam['fy'], cam['cv']], \
                      [0.0, 0.0, 1.0]]);
@@ -40,68 +34,17 @@ if __name__ == '__main__':
   framenum = 0;
   while True:
     framenum = framenum + 1;
-    (success, I) = video_reader.getNextFrame()
-    if success == False:
-      break
-    if framenum % 50 != 0:
-      continue
-
-    #roll_start = deg2rad(gps_dat[framenum,7]);
-    #pitch_start = -deg2rad(gps_dat[framenum,8]);
-    #yaw_start = deg2rad(gps_dat[framenum,9]);
-
-    pitch_start = deg2rad(gps_dat[framenum,8]);
-    roll_start = -deg2rad(gps_dat[framenum,7]);
-    yaw_start = -deg2rad(gps_dat[framenum,9] + 90);
-
-    psi = pitch_start; 
-    cp = cos(psi);
-    sp = sin(psi);
-    theta = roll_start;
-    ct = cos(theta);
-    st = sin(theta);
-    gamma = yaw_start;
-    cg = cos(gamma);
-    sg = sin(gamma);
-
-    #R_to_i_from_w = \
-    #euler_matrix(pitch_start,roll_start,yaw_start,'rxyz')[0:3,0:3]
-
-    #print euler_matrix(pitch_start,roll_start,yaw_start,'sxyz')[0:3,0:3]
-    R_to_i_from_w = \
-            array([[cg*cp-sg*st*sp, -sg*ct, cg*sp+sg*st*cp],
-                  [sg*cp+cg*st*sp, cg*ct, sg*sp-cg*st*cp],
-                  [-ct*sp, st, ct*cp]]).transpose()
+    #(success, I) = video_reader.getNextFrame()
+    #if success == False:
+    #  break
+    #if framenum % 50 != 0:
+    #  continue
     
-
-    # pts = zeros([num_imgs_fwd, 3])
-    # for t in range(num_imgs_fwd):
-    pts = zeros([min(gps_dat.shape[0]-framenum,5000),3])
-    for t in range(pts.shape[0]):
-      pts[t,:] = WGS84toENU(gps_dat[framenum, 1:4], gps_dat[framenum+t, 1:4])
-      pts[t,2] = pts[t,2] 
-
-    for idx in range(1,pts.shape[0]):
-      world_coordinates = pts[idx,:]
-      pos_wrt_imu = dot(R_to_i_from_w, world_coordinates)
-      #pos_wrt_camera = dot(R_to_c_from_i, (pos_wrt_imu - array([-0.25, -1.0,
-      # 0.6])))
-      #pos_wrt_camera = dot(R_to_c_from_i, (pos_wrt_imu + array([0, 0.5, -0.57])))
-      pos_wrt_camera = dot(R_to_c_from_i, (pos_wrt_imu + array([0, 0.0, -0])))
-      #pos_wrt_camera = dot(R_to_c_from_i, (pos_wrt_imu + array([-0.25, -2.13,
-      #  0])))
-      pos_wrt_camera[1] = pos_wrt_camera[1] + 1.1;
-      # quick hack added by Tao
-      #pos_wrt_camera[2] = pos_wrt_camera[2]*1.4
-      pix = around(dot(cam['KK'], divide(pos_wrt_camera, pos_wrt_camera[2])))
-      if (pix[0] > 0 and pix[0] < 1280 and pix[1] > 0 and pix[1] < 960):
-        I[pix[1]-0:pix[1]+1, pix[0]-0:pix[0]+1, 0] = 255;
-        I[pix[1]-0:pix[1]+1, pix[0]-0:pix[0]+1, 1] = 0;
-        I[pix[1]-0:pix[1]+1, pix[0]-0:pix[0]+1, 2] = 0;
-
-    imshow('video', I)
-    key = waitKey(5)
-    if key == 'q':
-      break;
+    M = GPSMask(gps_dat[framenum:framenum+num_imgs_fwd,:], cam); 
+    #I = np.minimum(M,I)
+    #imshow('video', I)
+    #key = waitKey(5)
+    #if key == 'q':
+    #  break;
 
 
