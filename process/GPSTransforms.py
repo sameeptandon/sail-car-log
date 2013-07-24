076817383be1d8e07c5041fc23bfc941844a0610
Nikhil Bhargava <nikhilb@stanford.edu>
2013-07-23 17:17:47 -0700

    label lanes based on gps outputs

---
+++ process/GPSTransforms.py
@@ -0,0 +1,56 @@
+from transformations import euler_matrix
+import numpy as np
+from WGS84toENU import *
+from numpy import array, dot, zeros, around, divide, ones
+
+def R_to_c_from_w(roll, pitch, yaw, cam):
+    cp = cos(pitch)
+    sp = sin(pitch)
+    ct = cos(roll)
+    st = sin(roll)
+    cg = cos(yaw)
+    sg = sin(yaw)
+
+    R_to_i_from_w = \
+            array([[cg*cp-sg*st*sp, -sg*ct, cg*sp+sg*st*cp],
+                  [sg*cp+cg*st*sp, cg*ct, sg*sp-cg*st*cp],
+                  [-ct*sp, st, ct*cp]]).transpose()
+
+    R_to_c_from_i = cam['R_to_c_from_i']
+    R_camera_pitch = euler_matrix(cam['rot_x'], cam['rot_y'], cam['rot_z'], 'sxyz')[0:3,0:3]
+    R_to_c_from_i = dot(R_camera_pitch, R_to_c_from_i)
+
+    return dot(R_to_c_from_i, R_to_i_from_w)
+
+
+def GPSTransforms(GPSData, Camera, width=2): 
+
+    tr = np.array([np.eye(4),]*GPSData.shape[0])
+
+    roll_start = -deg2rad(GPSData[0,7]);
+    pitch_start = deg2rad(GPSData[0,8]);
+    yaw_start = -deg2rad(GPSData[0,9]+90);
+
+    base_R_to_c_from_w = R_to_c_from_w(roll_start, pitch_start, yaw_start, Camera)
+    pts = WGS84toENU(GPSData[0,1:4], GPSData[:,1:4])
+
+    world_coordinates = pts;
+    pos_wrt_camera = dot(base_R_to_c_from_w, world_coordinates);
+
+    tr[:,0,3] = pos_wrt_camera[0,:]
+    tr[:,1,3] = pos_wrt_camera[1,:]
+    tr[:,2,3] = pos_wrt_camera[2,:]
+
+    for i in xrange(GPSData.shape[0]):
+        roll = -deg2rad(GPSData[i,7])
+        pitch = deg2rad(GPSData[i,8])
+        yaw = -deg2rad(GPSData[i,9] + 90)
+
+        rot = R_to_c_from_w(roll, pitch, yaw, Camera).transpose()
+
+        pos = pos_wrt_camera[0:3,i]
+        tr[i, 0:3,0:3] = dot(base_R_to_c_from_w, rot)
+
+    return tr
+
+
