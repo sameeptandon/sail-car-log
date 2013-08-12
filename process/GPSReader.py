228df22d118d5732b2d2fb4a3d3f484b93b6ac19
Nikhil Bhargava <nikhilb@stanford.edu>
2013-08-12 14:10:09 -0700

    smooth gps stuff

--- process/GPSReader.py
+++ process/GPSReader.py
@@ -1,4 +1,9 @@
 import numpy as np
+from scipy.ndimage.filters import convolve1d
+
+def smoothData(arr, wind=51):
+  filt = np.ones(wind) / wind
+  return convolve1d(arr, filt, mode='nearest')
 
 class GPSReader():
   def __init__(self, filename):
@@ -20,9 +25,18 @@ class GPSReader():
     return self.data;
 
   def getNumericData(self):
-    arr = np.zeros([len(self.data), 10]);
+    arr = np.zeros([len(self.data), 10], np.float64);
     for t in range(len(self.data)):
       for j in range(10):
         arr[t,j] = self.data[t][self.token_order[j]]
+
+    arr[:, 1] = smoothData(arr[:, 1])
+    arr[:, 2] = smoothData(arr[:, 2])
+    arr[:, 3] = smoothData(arr[:, 3])
+    arr[:, 7] = smoothData(arr[:, 7])
+    arr[:, 8] = smoothData(arr[:, 8])
+    arr[:, 9] = smoothData(arr[:, 9])
+
     return arr;
+
 
