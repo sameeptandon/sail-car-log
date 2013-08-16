f99fa7f16e3107596df2fcb5d2f480ec3271fb86
Nikhil Bhargava <nikhilb@stanford.edu>
2013-08-16 15:11:40 -0700

    remove arguably unneeded crap

--- process/GPSReprojection.py
+++ process/GPSReprojection.py
@@ -52,46 +52,7 @@ def PointsMask(pos_wrt_camera, Camera):
    
 
 def GPSMask(GPSData, Camera, width=2): 
-
-    roll_start = -deg2rad(GPSData[0,7]);
-    pitch_start = deg2rad(GPSData[0,8]);
-    yaw_start = -deg2rad(GPSData[0,9]+90);
-
-    psi = pitch_start; 
-    cp = cos(psi);
-    sp = sin(psi);
-    theta = roll_start;
-    ct = cos(theta);
-    st = sin(theta);
-    gamma = yaw_start;
-    cg = cos(gamma);
-    sg = sin(gamma);
-
-    R_to_i_from_w = \
-            array([[cg*cp-sg*st*sp, -sg*ct, cg*sp+sg*st*cp],
-                  [sg*cp+cg*st*sp, cg*ct, sg*sp-cg*st*cp],
-                  [-ct*sp, st, ct*cp]]).transpose()
-
-
-    pts = WGS84toENU(GPSData[0,1:4], GPSData[:,1:4])
-
-    R_to_c_from_i = Camera['R_to_c_from_i']
-    R_camera_pitch = euler_matrix(Camera['rot_x'], Camera['rot_y'],\
-            Camera['rot_z'], 'sxyz')[0:3,0:3]
-    R_to_c_from_i = dot(R_camera_pitch, R_to_c_from_i) 
-
-    #I = zeros((960,1280,3), np.uint8)
-    I = 255*ones((960,1280,3), np.uint8)
-
-    world_coordinates = pts;
-    pos_wrt_imu = dot(R_to_i_from_w, world_coordinates);
-    pos_wrt_camera = dot(R_to_c_from_i, pos_wrt_imu);
-
-    pos_wrt_camera[0,:] += Camera['t_x'] #move to left/right
-    pos_wrt_camera[1,:] += Camera['t_y'] #move up/down image
-    pos_wrt_camera[2,:] += Camera['t_z'] #move away from cam
-    vpix = around(dot(Camera['KK'], divide(pos_wrt_camera, pos_wrt_camera[2,:])))
-    vpix = vpix.astype(np.int32)
+    vpix = GPSColumns(GPSData, Camera, GPSData[0, :])
     vpix = vpix[:,vpix[0,:] > 0 + width/2]
     vpix = vpix[:,vpix[1,:] > 0 + width/2]
     vpix = vpix[:,vpix[0,:] < 1279 - width/2]
