6d4a58342c335be949b5f76cf269fa737453356d
Pranav Rajpurkar <pranav.rajpurkar@gmail.com>
2013-11-24 21:03:58 -0800

    partially implements handling of multiple drives

--- mapping/mapcreation/map.py
+++ mapping/mapcreation/map.py
@@ -1,29 +1,61 @@
 #implements the mapping system
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.optimize import minimize
 
-Data = np.array([[5,1], [4.5,2], [3.75, 2.5], [3,3], [2.3, 3.4], [2, 4], [1.7, 5], [1.8,6], [2.4, 7], [3,8]]);
-X = Data[:,0];
-Y = Data[:,1];
-m = X.size;
-assert m == Y.size
-a = np.ones(m);
-index = np.arange(X.size);
-b = np.vstack((a, index)).T
-tau = 0.5;
-lins = np.linspace(0., 10., 80)
-outputX = np.zeros(lins.size);
-outputY = np.zeros(lins.size);
-W = np.zeros([m, m]);
-for i in range(0, lins.size):
-	for l in range(0, m):
-		W[l,l] = np.exp(-np.power(lins[i] - b[l, 1],2)/(2*(tau**2)));
-	inter = np.linalg.pinv(b.T.dot(W).dot(b)).dot(b.T).dot(W);
-	thetaX = inter.dot(X);
-	thetaY = inter.dot(Y);
-	outputX[i] = thetaX[1]*lins[i] + thetaX[0];
-	outputY[i] = thetaY[1]*lins[i] + thetaY[0];
-#plt.scatter(lins, output,color = "y");
-plt.scatter(X, Y);
-plt.scatter(outputX, outputY, color = "r");
+#data - original and new
+data = np.array([[5,1], [4.5,2], [3.75, 2.5], [3,3], [2.3, 3.4], [2, 4], [1.7, 5], [1.8,6], [2.4, 7], [3,8]]);
+newData = np.array([[2.5, 3.1], [2.5, 7.5], [4.0, 2.4]]);
+
+#we parameterize by lambdas
+def getLambdas(data):
+	ones = np.ones(data.shape[0]);
+	return np.vstack((ones, np.arange(data.shape[0]))).T
+
+#given as input a lambda, the function outputs its best prediction for the
+#location of the point.
+def predict(lam, datalambdas):
+	m = data.shape[0];
+	W = np.zeros([m,m]);
+	tau = 0.5;
+	for i in range(0, m):
+		W[i,i] = np.exp(-((lam - datalambdas[i, 1])**2)/(2*(tau**2)));
+		#intermediary result to allow calc of both X and Y without redundancy
+		inter =	np.linalg.pinv(datalambdas.T.dot(W).dot(datalambdas)).dot(datalambdas.T).dot(W);
+	[thetaX, thetaY] = inter.dot(data[:, 0]), inter.dot(data[:,1]);
+	[estimateX, estimateY] = [thetaX[1]*lam + thetaX[0], thetaY[1]*lam + thetaY[0]];
+	return [estimateX, estimateY];
+
+#this is the objective function we minimize over. It is the sum of squares of
+#x difference and y difference
+def minf(lam, coord, datalambdas):
+	[estimateX, estimateY] = predict(lam, datalambdas)
+	return (coord[0] - estimateX)**2 + (coord[1] - estimateY)**2;
+
+#call predict on a list of lambdas, returning an array of coordinates
+def fit(lambdas):
+	predtest = np.zeros([lambdas.size, 2]);
+	datalambs = getLambdas(data)
+	for i in range(0, lambdas.size):
+		predtest[i] = predict(lambdas[i], datalambs)
+	return predtest;
+
+def addToMap(newcoords):
+	for newcoord in newcoords:
+		minimized = minimize(minf, 0, (newcoord, getLambdas(data)));
+		indexToAdd = np.ceil(minimized.x[0]);
+		data = np.insert(data, indexToAdd, newcoord, axis =0);
+
+def drawMap(lambdas, color):
+	predCoords = fit(lambdas);
+	plt.scatter(predCoords[:0], predCoords[:,1], color= color);
+
+#graph original data points
+plt.scatter(data[:,0], data[:,1]);
+
+#checking fit on original data points
+test = np.linspace(0., 10., 80)
+predCoords = fit(test)
+plt.scatter(predCoords[:,0], predCoords[:,1], color = "r");
+addToMap(newData);
 plt.show();
