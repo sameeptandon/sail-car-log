025ee2b75e90de233451473d7ce2c29c6a9ba369
Sameep Tandon <sameep@stanford.edu>
2014-02-12 17:36:56 -0800

    improvements on calibration

--- process/LidarCalibration.py
+++ process/LidarCalibration.py
@@ -35,7 +35,7 @@ def computeLogDistanceTransformSlow(D, gamma):
         for y in reversed(range(height-1)):
             D[x,y] = max(D[x,y], D[x+1,y]+lg, D[x,y+1]+lg, D[x+1,y+1]+lg)
     
-    print D
+    #print D
     return D
 
 def computeLogDistanceTransform(D, gamma): 
@@ -87,10 +87,10 @@ def generateEdgeFilterKernels():
 
 def processPointCloud(raw_pts): 
     # add rotational angle and distance to pts
-    pts = np.zeros((raw_pts.shape[0], raw_pts.shape[1]+2))
+    pts = np.zeros((raw_pts.shape[0], raw_pts.shape[1]+2), dtype=np.float32)
     pts[:,:-2] = raw_pts
-    pts[:,-2] = np.arctan2(pts[:,2], pts[:,1]) + np.pi
-    pts[:,-1] = np.sum( pts[:, 0:3] ** 2, axis=1 ) 
+    pts[:,-2] = np.arctan2(pts[:,1], pts[:,0]) + np.pi
+    pts[:,-1] = np.sqrt(np.sum( pts[:, 0:3] ** 2, axis=1 )) 
 
     pts = pts[ pts[:,5].argsort() ] # sort on rotational angle
     pts = pts[ pts[:,4].argsort(kind='mergesort') ] # stable sort on laser num
@@ -99,21 +99,21 @@ def processPointCloud(raw_pts):
     pts[-1,3] = 0.0
     for idx in range(1,pts.shape[0]-1):
         if pts[idx,4] == pts[idx-1,4] and pts[idx,4] == pts[idx+1,4]: 
-            pts[idx,3] = max(pts[idx-1,6] - pts[idx,6], 
-                             pts[idx+1,6] - pts[idx,6],
-                             0) ** (0.5)
+            pts[idx,3] = max(pts[idx-1,-1] - pts[idx,-1], 
+                             pts[idx+1,-1] - pts[idx,-1],
+                             0)
         else:
             pts[idx,3] = 0.0
     return pts
 
 
-def computeReprojection(C, pts, cam):
-    raw_pts = pts[:, 0:3].copy()
-    raw_pts[:, 0] += C[0]
-    raw_pts[:, 1] += C[1]
-    raw_pts[:, 2] += C[2]
+def computeReprojection(C, raw_pts, cam):
+    pts = raw_pts[:, 0:3].copy()
+    pts[:, 0] += C[0]
+    pts[:, 1] += C[1]
+    pts[:, 2] += C[2]
     R = euler_matrix(C[3], C[4], C[5])[0:3,0:3]
-    pts_wrt_cam = dot(R, dot(R_to_c_from_l(cam), raw_pts.transpose()))
+    pts_wrt_cam = dot(R, dot(R_to_c_from_l(cam), pts.transpose()))
     pix = np.around(np.dot(cam['KK'], np.divide(pts_wrt_cam[0:3,:], pts_wrt_cam[2, :])))
     pix = pix.astype(np.int32)
     return (pix, pts_wrt_cam)
@@ -136,6 +136,35 @@ def computeReprojectionScore(C, pts, I, cam):
     py = pix[0,mask]
     return -np.sum(I[px,py])
 
+
+def gridsearch(C, batch, cam):
+    m = range(-1,2)
+    step_t = 0.01
+    step_r = 0.001
+
+    best_score = float("inf")
+    best_d = None
+
+    for dtx in m:
+        for dty in m:
+            for dtz in m:
+                for drx in m:
+                    for dry in m:
+                        for drz in m:
+                            d = np.array([step_t*dtx, step_t*dty, step_t*dtz, step_r*drx, step_r*dry, step_r*drz])
+                            C_new = C + d
+                            score = 0
+                            for p in batch:
+                                E = p[2]
+                                proc_pts = p[3]
+                                score = score + computeReprojectionScore(C_new, proc_pts, E, cam)
+                            if score < best_score:
+                                best_score = score
+                                best_d = d
+    return (C+best_d, best_score)
+
+
+
 def drawReprojection(C, pts, I, cam, colorMap=False):
     (pix, pts_wrt_cam) = computeReprojection(C, pts, cam)
     mask = computeMask(pix, pts_wrt_cam)
@@ -155,7 +184,48 @@ def drawReprojection(C, pts, I, cam, colorMap=False):
             I[px,np.maximum(py-p,0)] = 255
         
     imshow('display', I)
-    waitKey()
+    waitKey(10)
+
+def getNextData(reader, LDRFrameMap):
+    for idx in range(25):
+        (success, img) = reader.getNextFrame()
+        if not success:
+            return None
+    ldr_frame = loadLDR(LDRFrameMap[reader.framenum])
+    #print LDRFrameMap[reader.framenum]
+    return (success, img, ldr_frame)
+  
+
+def processData(data):
+    I, pts = data
+    E = processImage(I);
+    proc_pts = processPointCloud(pts)
+    proc_pts = proc_pts[ proc_pts[:, 3] > 0.5, :]
+    return [I, pts, E, proc_pts]
+
+def processBatch(batch):
+    processed = [ ]
+    count = 0
+    for data in batch:
+        print 'Processing:', count, 'out of', len(batch)
+        count += 1
+        output = processData(data)
+        processed.append(output)
+    return processed
+
+kernels = generateEdgeFilterKernels()
+def processImage(I):
+    # convert the image to grayscale
+    E = cv2.cvtColor(I, cv2.COLOR_BGR2GRAY)
+
+    # run an edge filter
+    edges = cv2.filter2D(E, cv2.CV_8U, np.zeros((1,1)))
+    
+    for k in kernels: 
+        edges = np.maximum(edges, np.abs(cv2.filter2D(E, cv2.CV_8U, k)))
+
+    edges = computeDistanceTransform(edges+1, 0.95, 1.0/3)
+    return edges
  
 if __name__ == '__main__': 
     video_filename = sys.argv[1] 
@@ -163,48 +233,61 @@ if __name__ == '__main__':
     vidname = vfname.split('.')[0]
     cam_num = int(vidname[-1])
     cam = getCameraParams()[cam_num - 1] 
-    video_reader = VideoReader(video_filename,num_splits=1) # require only split_0 
-    pts = loadLDR(sys.argv[2])
-    pts = processPointCloud(pts)
-    pts = pts[ pts[:, 3] > 0.5, :]
+    video_reader = VideoReader(video_filename)
+    ldr_frame_map = loadLDRCamMap(sys.argv[2])
+    (tx,ty,tz,rx,ry,rz) = (-0.30000000000000004, 0.5, 0.4299999999999998, -0.09800000000000006, 0.019999999999999987, 0.010999999999999982)
+    (tx,ty,tz,rx,ry,rz) = (-0.35493086,  0.49796525,  0.43775339, -0.0986506, 0.01898486,  0.01463721)
+    C_current = array([tx,ty,tz,rx,ry,rz])
+    BATCH_SIZE = 50
 
-    # translate points in lidar frame to camera frame
-    tx = -0.0
-    ty = 0.1
-    tz = -0.0
-    rx = 0.0
-    ry = 0.0
-    rz = 0.0
-    C_init = array([tx,ty,tz,rx,ry,rz])
-    count = 0 
-    while count < 10:
-        (success, I) = video_reader.getNextFrame()
-        count += 1
-    # convert the image to grayscale
-    E = cv2.cvtColor(I, cv2.COLOR_BGR2GRAY)
+    from multiprocessing import Pool
+    pool = Pool(8)
 
-    # run an edge filter
-    kernels = generateEdgeFilterKernels()
-    edges = cv2.filter2D(E, cv2.CV_8U, np.zeros((1,1)))
+    while True:
+        
+        batch_data = [ ]
+        while len(batch_data) < BATCH_SIZE:
+            (success, I, raw_pts) = getNextData(video_reader, ldr_frame_map)
+            if not success:
+                break
+            batch_data.append( [I, raw_pts] )
+
+        batch_data = pool.map(processData, batch_data)
+        #batch_data = processBatch(batch_data)
+       
+        count = 0
+        while count < 10:
+            count +=1
+
+
+            #cv2.imshow('edges', edges/255.0)
+            """
+            from scipy.optimize import brute
     
-    for k in kernels: 
-        edges = np.maximum(edges, np.abs(cv2.filter2D(E, cv2.CV_8U, k)))
+            step_size = 0.0001
+            step_sizes = [0.0005, 0.0005, 0.0005, 0.0001, 0.0001, 0.0001]
+            ranges = [(x-y, x+y) for x,y in zip(C_current, step_sizes)]
+            print ranges
+            out = brute(computeReprojectionScore, ranges, args=(pts, edges, cam), full_output=True, Ns=1)
+            """
+            out = gridsearch(C_current, batch_data, cam)
+            #print out[0]
+            #print out[1]
+            #print counter
+            #drawReprojection(out[0], pts, E, cam)
+            E = batch_data[0][2]
+            proc_pts = batch_data[0][3]
 
-    #edges = cv2.blur(edges, (30,30))
-    edges = computeDistanceTransform(edges+1, 0.98, 1.0/3)
-    imshow('display', edges/255.0)
-    waitKey()
-
-    from scipy.optimize import brute
-    step_size = 0.001
-    ranges = [(x-step_size, x+step_size) for x in C_init]
-    out = brute(computeReprojectionScore, ranges, args=(pts, edges, cam), full_output=True, Ns=3) 
-    print out[0]
-    print out[1]
-    print counter
-    drawReprojection(C_init, pts, E, cam)
-    drawReprojection(out[0], pts, E, cam)
-    drawReprojection(out[0], loadLDR(sys.argv[2]), I, cam, colorMap=True)
+            (pix, pts_wrt_cam) = computeReprojection(C_current, proc_pts, cam)
+            mask = computeMask(pix, pts_wrt_cam)
+            px = pix[1,mask]
+            py = pix[0,mask]
+            edges = E.copy()
+            edges[px,py] = 255
+            imshow('score', edges/255.0)
+            waitKey(10)
+            C_current = out[0]
+            print C_current
     
-    #imshow('display', I)
-    #key = chr((waitKey() & 255))
+            #imshow('display', I)
+            #key = chr((waitKey() & 255))
--- process/LidarReprojection.py
+++ process/LidarReprojection.py
@@ -8,6 +8,16 @@ from ColorMap import *
 from numpy import exp
 from transformations import euler_matrix
 
+def getNextData(VideoReader, LDRFrameMap):
+    for idx in range(5):
+        (success, img) = VideoReader.getNextFrame()
+        if not success:
+            return None
+    ldr_frame = loadLDR(LDRFrameMap[VideoReader.framenum])
+    print LDRFrameMap[VideoReader.framenum]
+    return (success, img, ldr_frame)
+
+
 if __name__ == '__main__': 
     video_filename = sys.argv[1] 
     path, vfname = os.path.split(video_filename)
@@ -15,24 +25,34 @@ if __name__ == '__main__':
     cam_num = int(vidname[-1])
     cam = getCameraParams()[cam_num - 1] 
     video_reader = VideoReader(video_filename,num_splits=1) # require only split_0 
-    pts = loadLDR(sys.argv[2])
 
-    width = 1
+    width = 9
     # translate points in lidar frame to camera frame
-    tx = -0.00
-    ty = 0.1
-    tz = -0.0
-    rx = 0.0
-    ry = 0.0
-    rz = 0.0
-    count = 0 
-    while count < 10:
-        (success, I) = video_reader.getNextFrame()
-        count += 1
 
+    #(-0.20000000000000007, 0.4, 0.32999999999999985, -0.09800000000000006, 0.014999999999999986, -0.02400000000000002)
+    #(-0.10000000000000007, 0.5, 0.4299999999999998, -0.09800000000000006, 0.019999999999999987, 0.000999999999999982)
+    (tx, ty, tz, rx, ry, rz) = \
+            [-0.30493086,  0.41796525,  0.49775339, -0.1036506,   0.01598486,  0.00963721]
+
+            #(-0.30000000000000004, 0.5, 0.4299999999999998, -0.09800000000000006, 0.019999999999999987, 0.010999999999999982)
+    video_reader = VideoReader(video_filename)
+    ldr_frame_map = loadLDRCamMap(sys.argv[2])
+    
+    """
+    count = 0
+    while count < 230:
+        (success, I, pts) = getNextData(video_reader, ldr_frame_map)
+        count += 1
     orig = I.copy()
+    """
+
     while True:
+        (success, I, pts) = getNextData(video_reader, ldr_frame_map)
+        if not success: 
+            break
+        orig = I.copy()
         I = orig.copy()
+        #pts = pts[pts[:,3] > 45,:]
         raw_pts = array(pts[:, 0:3])
         raw_pts[:, 0] += tx
         raw_pts[:, 1] += ty
@@ -52,47 +72,45 @@ if __name__ == '__main__':
 
         dist_wrt_cam = np.sum( pts[mask, 0:3] ** 2, axis=1 ) ** (1.0/2)
         intensity = pts[mask, 3]
+        
         #colors = heatColorMapFast(dist_wrt_cam, 0, np.max(dist_wrt_cam))
         #colors = heatColorMapFast(exp(-dist_wrt_cam/3), 0, 1)
-        colors = heatColorMapFast(intensity, 0, 255)
+        colors = heatColorMapFast(intensity, 0, 100)
 
         px = pix[1,mask]
         py = pix[0,mask]
         I[px, py, :] = colors[0,:,:]
-        """
-        for p in range(-width/2,width/2):
+        for p in range(0,width/2):
             I[px+p,py, :] = colors[0,:,:] 
             I[px,py+p, :] = colors[0,:,:]
             I[px-p,py, :] = colors[0,:,:]
             I[px,py-p, :] = colors[0,:,:]
-        """
         imshow('display', I)
-        key = chr((waitKey() & 255))
-        
-        if key == 'w':
-            tx += 0.001
-        elif key == 'e':
-            tx -= 0.001
-        elif key == 'r':
-            ty += 0.001
-        elif key == 't':
-            ty -= 0.001
-        elif key == 'y':
-            tz += 0.001
-        elif key == 'u':
-            tz -= 0.001
+        key = chr((waitKey(1) & 255))
+        if key == '+':
+            tx += 0.05
+        elif key == '_':
+            tx -= 0.05
         elif key == 'a':
-            rx += 0.001
-        elif key == 's':
-            rx -= 0.001
+            ty += 0.05
         elif key == 'd':
-            ry += 0.001
-        elif key == 'f':
-            ry -= 0.001
-        elif key == 'g':
-            rz += 0.001
-        elif key == 'h':
-            rz -= 0.001
+            ty -= 0.05
+        elif key == 'w':
+            tz += 0.05
+        elif key == 's':
+            tz -= 0.05
         
+        elif key == 'i':
+            rx += 0.005
+        elif key == 'k':
+            rx -= 0.005
+        elif key == 'l':
+            ry += 0.005
+        elif key == 'j':
+            ry -= 0.005
+        elif key == 'o':
+            rz += 0.005
+        elif key == 'u':
+            rz -= 0.005
 
         print (tx, ty, tz, rx, ry, rz)
