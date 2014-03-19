76672b7a1dc83c0f89bb7e0d81e0486c8397fe23
Sameep Tandon <sameep@stanford.edu>
2014-03-19 11:57:32 -0700

    cleaning up code; note the change in LidarTransforms.py : R_to_c_from_l might actually break pre-existing code. Please update your old code accordingly (see LidarIntegrator.py for an example)

--- process/LidarIntegator.py
+++ process/LidarIntegator.py
@@ -11,7 +11,6 @@ import numpy as np
 from ColorMap import *
 import vtk
 import copy
-from CameraParams import * 
 import cv2
 
 
@@ -33,8 +32,6 @@ clouds = [ ]
 all_data = [ ] 
 cloud_r = vtk.vtkRenderer()
 renderWindow = vtk.vtkRenderWindow()
-(rx,ry,rz) = (0,-0.015,-0.045)
-R = euler_matrix(rx,ry,rz)[0:3,0:3]
 
 # settings for 280N_e 
 #start_fn = 1200 
@@ -47,19 +44,15 @@ R = euler_matrix(rx,ry,rz)[0:3,0:3]
 #step = 5
 
 # settings for parking
-#start_fn = 0
-#num_fn = 200
-#step = 2
-
 start_fn = 0
-num_fn = 10
-step = 9
+num_fn = 200
+step = 2
+
+#start_fn = 7500
+#num_fn = 300
+#step = 3
 
 color_mode = 'INTENSITY'
-
-(ctx, cty, ctz, crx, cry, crz) = \
-        (-0.0, 0.30999999999999994, 0.15, 0.04899999999999999, 0.015999999999999997, 0.014000000000000005)
-cR = euler_matrix(crx, cry, crz)[0:3,0:3]
 
 def cloudToPixels(cam, pts_wrt_cam): 
 
@@ -101,18 +94,14 @@ def loadImage(video_reader, framenum):
     (success, I) = video_reader.getNextFrame()
     return (success, I)
 
-def integrateClouds(ldr_map, IMUTransforms, renderer, R, offset, num_steps, step):
+def integrateClouds(ldr_map, IMUTransforms, renderer, offset, num_steps, step):
     start = offset
     end = offset + num_steps*step
 
-    video_reader.setFrame(start)
+    video_reader1.setFrame(start)
+    video_reader2.setFrame(start)
 
-    T_from_l_to_i = np.eye(4)
-    T_from_l_to_i[0:3,0:3] = R.transpose()
-    
     trans_wrt_imu = IMUTransforms[start:end,0:3,3]
-    #trans_wrt_lidar = np.dot(R, trans_wrt_imu.transpose())
-    #trans_wrt_lidar = trans_wrt_lidar.transpose()
     gpsPointCloud = VtkPointCloud(trans_wrt_imu[:,0:3], 0*trans_wrt_imu[:,0])
     clouds.append(gpsPointCloud)
     actors.append(gpsPointCloud.get_vtk_cloud())
@@ -131,30 +120,42 @@ def integrateClouds(ldr_map, IMUTransforms, renderer, R, offset, num_steps, step
         #data = data[ data[:,2] < -1.5]
         #data = data[ data[:,2] > -2.5]
         
-        (success, I) = video_reader.getNextFrame()
-        stepVideo(video_reader, step)
+        (success, I1) = video_reader1.getNextFrame()
+        (success, I2) = video_reader2.getNextFrame()
+        stepVideo(video_reader1, step)
+        stepVideo(video_reader2, step)
         
-        pts_wrt_cam = array(data[:, 0:3])
-        pts_wrt_cam[:, 0] += ctx
-        pts_wrt_cam[:, 1] += cty
-        pts_wrt_cam[:, 2] += ctz
-        pts_wrt_cam = dot(cR, dot(R_to_c_from_l(cam), pts_wrt_cam.transpose()))
-        (pix, mask) = cloudToPixels(cam, pts_wrt_cam)
+        pts_wrt_cam1 = array(data[:, 0:3])
+        pts_wrt_cam1[:, 0:3] += cam1['displacement_from_l_to_c_in_lidar_frame'];
+        pts_wrt_cam1 = dot(R_to_c_from_l(cam1), pts_wrt_cam1.transpose())
+        (pix1, mask1) = cloudToPixels(cam1, pts_wrt_cam1)
 
-        #intensity = data[mask, 3]
-        #colors = heatColorMapFast(intensity, 0, 100)
-        #I[pix[1,mask], pix[0,mask], :] = colors[0,:,:]
         
-        colors = 0*pts_wrt_cam.transpose() + 0
-        print I[pix[1,mask], pix[0,mask], :].shape
-        print colors[mask, :].shape
+        colors = 0*pts_wrt_cam1.transpose() + 0
+        print I1[pix1[1,mask1], pix1[0,mask1], :].shape
+        print colors[mask1, :].shape
+        
         # BGR -> RGB
-        colors[ mask, 0] = I[pix[1,mask], pix[0,mask], 2] 
-        colors[ mask, 1] = I[pix[1,mask], pix[0,mask], 1] 
-        colors[ mask, 2] = I[pix[1,mask], pix[0,mask], 0] 
+        colors[ mask1, 0] = I1[pix1[1,mask1], pix1[0,mask1], 2] 
+        colors[ mask1, 1] = I1[pix1[1,mask1], pix1[0,mask1], 1] 
+        colors[ mask1, 2] = I1[pix1[1,mask1], pix1[0,mask1], 0] 
 
-        #cv2.imshow('vid', I)
-        #cv2.waitKey(5)
+        pts_wrt_cam2 = array(data[:, 0:3])
+        pts_wrt_cam2[:, 0:3] += cam2['displacement_from_l_to_c_in_lidar_frame'];
+        pts_wrt_cam2 = dot(R_to_c_from_l(cam2), pts_wrt_cam2.transpose())
+        (pix2, mask2) = cloudToPixels(cam2, pts_wrt_cam2)
+
+        # BGR -> RGB
+        colors[ mask2, 0] = I2[pix2[1,mask2], pix2[0,mask2], 2] 
+        colors[ mask2, 1] = I2[pix2[1,mask2], pix2[0,mask2], 1] 
+        colors[ mask2, 2] = I2[pix2[1,mask2], pix2[0,mask2], 0] 
+        
+        #intensity = data[mask2, 3]
+        #heat_colors = heatColorMapFast(intensity, 0, 100)
+        #I2[pix2[1,mask2], pix2[0,mask2], :] = heat_colors[0,:,:]
+
+        cv2.imshow('vid', I2)
+        cv2.waitKey(5)
 
         # transform data into IMU frame
         pts = data[:,0:3].transpose()
@@ -171,7 +172,7 @@ def integrateClouds(ldr_map, IMUTransforms, renderer, R, offset, num_steps, step
 
 
         if color_mode == 'CAMERA':
-            lidarCloud = VtkPointCloud(pts[mask,0:3], colors[mask,:])
+            lidarCloud = VtkPointCloud(pts[ (mask1 | mask2) ,0:3], colors[ (mask1 | mask2),:])
             actors.append(lidarCloud.get_vtk_color_cloud())
         elif color_mode == 'INTENSITY': 
             lidarCloud = VtkPointCloud(pts[:,0:3], data[:,3])
@@ -219,8 +220,6 @@ def keypress(obj, event):
 
     else:
         rerender = False
-    R = euler_matrix(rx,ry,rz)[0:3,0:3]
-
     if rerender:
         for a in actors:
             cloud_r.RemoveActor(a)
@@ -228,20 +227,23 @@ def keypress(obj, event):
         clouds = [ ]
         all_data = [ ]
         #start_fn = start_fn + 5
-        integrateClouds(ldr_map, imu_transforms, cloud_r, R, start_fn, num_fn, step)
+        integrateClouds(ldr_map, imu_transforms, cloud_r, start_fn, num_fn, step)
         renderWindow.Render()
     print key
-    print (rx, ry, rz)
 
 if __name__ == '__main__': 
-    video_filename = sys.argv[1] 
-    path, vfname = os.path.split(video_filename)
+    video_filename1 = sys.argv[1] 
+    path, vfname = os.path.split(video_filename1)
     vidname = vfname.split('.')[0]
+    vidname2 = vidname[:-1] + '2'
+    video_filename2 = path + '/' + vidname2 + '.avi'
     cam_num = int(vidname[-1])
     gps_filename = path + '/' + vidname[0:-1] + '_gps.out'
     
-    cam = getCameraParams()[cam_num - 1] 
-    video_reader = VideoReader(video_filename)
+    cam1 = GetQ50CameraParams()[0] 
+    cam2 = GetQ50CameraParams()[1] 
+    video_reader1 = VideoReader(video_filename1)
+    video_reader2 = VideoReader(video_filename2)
     gps_reader = GPSReader(gps_filename)
     GPSData = gps_reader.getNumericData()
     imu_transforms = IMUTransforms(GPSData)
@@ -251,7 +253,7 @@ if __name__ == '__main__':
     
     cloud_r.SetBackground(0., 0., 0.)
     cloud_r.SetViewport(0,0,1.0,1.0)
-    integrateClouds(ldr_map, imu_transforms, cloud_r, R, start_fn, num_fn, step)
+    integrateClouds(ldr_map, imu_transforms, cloud_r, start_fn, num_fn, step)
 
     # Render Window
     renderWindow = vtk.vtkRenderWindow()
