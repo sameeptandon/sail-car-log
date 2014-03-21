8c7b2a5665e7426799180c4d7c4d326f1445da9f
Sameep Tandon <sameep@stanford.edu>
2014-03-21 10:21:50 -0700

    disabling GPS smoothing. adding export options to lidarintegrator

--- process/LidarIntegrator.py
+++ process/LidarIntegrator.py
@@ -1,3 +1,9 @@
+# usage: 
+# python LidarIntegrator.py <dir> <basename><camnum>.avi <export name>.npz <optional additional flags such as --export or --full>
+
+# to change the type of data exported, see the function integrateClouds 
+
+
 from Q50_config import *
 from ArgParser import *
 import sys, os
@@ -52,11 +58,19 @@ renderWindow = vtk.vtkRenderWindow()
 #num_fn = 200
 #step = 2
 
-start_fn = 0
-num_fn = 250
-step = 10
+start_fn = 0 # offset in frame numbers to start exporting data
+num_fn = 250 # number of frames to export. this is changed if --full is enabled
+step = 10 # step between frames
 
 color_mode = 'INTENSITY'
+
+def exportData():
+        print 'exporting data'
+        export_data = np.row_stack(all_data)
+        print export_data
+        print export_data.shape
+        np.savez_compressed(sys.argv[3], data=export_data)
+        print 'export complete'
 
 def cloudToPixels(cam, pts_wrt_cam): 
 
@@ -117,12 +131,32 @@ def integrateClouds(ldr_map, IMUTransforms, renderer, offset, num_steps, step):
         data = loadLDR(ldr_map[fnum])
         # filter out the roof rack
         dist = np.sqrt(np.sum( data[:, 0:3] ** 2, axis = 1))
+
+        data_filter_mask = (dist > 3)                  & \
+                           (data[:,3] > 40)            & \
+                           (data[:,0] > 0)             & \
+                           (np.abs(data[:,1]) < 2.2)   & \
+                           (np.abs(data[:,1]) > 1.2)   & \
+                           (data[:,2] < -1.5)          & \
+                           (data[:,2] > -2.5)          
+
+        data = data[data_filter_mask, :]
+        """
         data = data[ dist > 3, :]
-        data = data[ data[:,3] > 60 ] 
-        #data = data[ data[:,0] > 0 ]
-        #data = data[ np.abs(data[:,1]) < 2.2]
-        #data = data[ data[:,2] < -1.5]
-        #data = data[ data[:,2] > -2.5]
+        # filter out on intensity
+        data = data[ data[:,3] > 60 , :]
+        # only points ahead of the car
+        data = data[ data[:,0] > 0, :]
+        # within 2.2 meters laterally of the lidar
+        data = data[ np.abs(data[:,1]) < 2.2]
+
+        # farther than 1.2 meters laterally of the lidar
+        data = data[ np.abs(data[:,1]) > 1.2]
+
+        # between -2.5 and -1.5 meters below the lidar
+        data = data[ data[:,2] < -1.5]
+        data = data[ data[:,2] > -2.5]
+        """
 
         if color_mode == 'CAMERA':
         
@@ -163,12 +197,13 @@ def integrateClouds(ldr_map, IMUTransforms, renderer, offset, num_steps, step):
             #cv2.imshow('vid', I2)
             #cv2.waitKey(5)
 
-        # transform data into IMU frame
+        # transform data into IMU frame at time t
         pts = data[:,0:3].transpose()
         pts = np.vstack((pts,np.ones((1,pts.shape[1]))))
         #R = euler_matrix(rx,ry,rz)[0:3,0:3].transpose()
         #T_from_l_to_i[0:3,0:3] = R
         pts = np.dot(T_from_l_to_i, pts)
+        # transform data into imu_0 frame
         pts = np.dot(IMUTransforms[fnum,:,:], pts);
         pts = pts.transpose()
 
@@ -220,11 +255,7 @@ def keypress(obj, event):
     elif key == 'v':
         color_mode = 'INTENSITY'
     elif key == 'x':
-        print 'exporting data'
-        export_data = np.row_stack(all_data)
-        print export_data
-        print export_data.shape
-        np.savez_compressed(sys.argv[3], data=export_data)
+        exportData()
 
     else:
         rerender = False
@@ -258,11 +289,23 @@ if __name__ == '__main__':
     imu_transforms = IMUTransforms(GPSData)
     ldr_map = loadLDRCamMap(args['map'])
 
+    
+    if '--full' in sys.argv:
+      total_num_frames = GPSData.shape[0]
+      start_fn = 0
+      step = 10
+      num_fn = int(total_num_frames / step)
+
+
     # this has been flipped for the q50
     
     cloud_r.SetBackground(0., 0., 0.)
     cloud_r.SetViewport(0,0,1.0,1.0)
     integrateClouds(ldr_map, imu_transforms, cloud_r, start_fn, num_fn, step)
+
+    if '--export' in sys.argv:
+      exportData()
+      sys.exit(0)
 
     # Render Window
     renderWindow = vtk.vtkRenderWindow()
@@ -273,8 +316,8 @@ if __name__ == '__main__':
     axisActor.SetGridlineXLength(5)
     axisActor.SetGridlineYLength(5)
     axisActor.SetGridlineZLength(5)
-    axisActor.DrawGridpolysOn()
-    axisActor.DrawInnerGridlinesOn()
+    #axisActor.DrawGridpolysOn()
+    #axisActor.DrawInnerGridlinesOn()
     cloud_r.AddActor(axisActor)
 
     # Interactor
