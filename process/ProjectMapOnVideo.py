04234bac8680c0b1931abb45426b838043030e0b
Sameep Tandon <sameep@stanford.edu>
2014-03-20 18:20:49 -0700

    cleaning up project map

--- process/ProjectMapOnVideo.py
+++ process/ProjectMapOnVideo.py
@@ -11,7 +11,7 @@ import numpy as np
 import cv2
 from ArgParser import *
 
-WINDOW = 50*10
+WINDOW = 50*5
 
 def cloudToPixels(cam, pts_wrt_cam): 
     width = 4
@@ -24,6 +24,24 @@ def cloudToPixels(cam, pts_wrt_cam):
     mask = np.logical_and(mask, pts_wrt_cam[2,:] > 0)
     dist_sqr = np.sum( pts_wrt_cam[0:3, :] ** 2, axis = 0)
     mask = np.logical_and(mask, dist_sqr > 3)
+
+    return (pix, mask)
+
+def localMapToPixels(map_data, imu_transforms_t, T_from_i_to_l, cam):
+    # load nearby map frames
+    pts_wrt_imu_0 = array(map_data[:,0:3]).transpose()
+    pts_wrt_imu_0 = np.vstack((pts_wrt_imu_0, 
+        np.ones((1,pts_wrt_imu_0.shape[1]))))
+    # transform points from imu_0 to imu_t
+    pts_wrt_imu_t = np.dot( np.linalg.inv(imu_transforms_t), pts_wrt_imu_0)
+    # transform points from imu_t to lidar_t
+    pts_wrt_lidar_t = np.dot(T_from_i_to_l, pts_wrt_imu_t);
+    # transform points from lidar_t to camera_t
+    pts_wrt_camera_t = pts_wrt_lidar_t.transpose()[:, 0:3] + cam['displacement_from_l_to_c_in_lidar_frame']
+    pts_wrt_camera_t = dot(R_to_c_from_l(cam), 
+            pts_wrt_camera_t.transpose())
+    # reproject camera_t points in camera frame
+    (pix, mask) = cloudToPixels(cam, pts_wrt_camera_t)
 
     return (pix, mask)
 
@@ -50,25 +68,13 @@ if __name__ == '__main__':
         for count in range(5):
             (success, I) = video_reader.getNextFrame()
         t = video_reader.framenum - 1
-        mask_window = (map_data[:,4] < t + WINDOW) & (map_data[:,4] > t - WINDOW);
+        mask_window = (map_data[:,4] < t + WINDOW) & (map_data[:,4] > t );
         map_data_copy = array(map_data[mask_window, :]);
 
-        # load nearby map frames
-        pts_wrt_imu_0 = array(map_data_copy[:,0:3]).transpose()
-        pts_wrt_imu_0 = np.vstack((pts_wrt_imu_0, 
-            np.ones((1,pts_wrt_imu_0.shape[1]))))
-        # transform points from imu_0 to imu_t
-        pts_wrt_imu_t = np.dot( np.linalg.inv(imu_transforms[t,:,:]), pts_wrt_imu_0)
-        # transform points from imu_t to lidar_t
-        pts_wrt_lidar_t = np.dot(T_from_i_to_l, pts_wrt_imu_t);
-        # transform points from lidar_t to camera_t
-        pts_wrt_camera_t = pts_wrt_lidar_t.transpose()[:, 0:3] + cam['displacement_from_l_to_c_in_lidar_frame']
-        pts_wrt_camera_t = dot(R_to_c_from_l(cam), 
-                pts_wrt_camera_t.transpose())
-        # reproject camera_t points in camera frame
-        (pix, mask) = cloudToPixels(cam, pts_wrt_camera_t)
+        # reproject
+        (pix, mask) = localMapToPixels(map_data_copy, imu_transforms[t,:,:], T_from_i_to_l, cam); 
 
-        # draw them
+        # draw 
         intensity = map_data_copy[mask, 3]
         heat_colors = heatColorMapFast(intensity, 0, 100)
         for p in range(4):
