96387d82ed70bb342097330ea3b64ad47df9ae5b
Sameep Tandon <sameep@stanford.edu>
2014-03-26 12:57:02 -0700

    cleaning pointgrey_image_writer

--- ros_drivers/src/pointgrey_image_viewer/pointgrey_image_writer_node.cpp
+++ ros_drivers/src/pointgrey_image_viewer/pointgrey_image_writer_node.cpp
@@ -11,8 +11,6 @@
 
 #define NUM_SPLITS 10
 
-static const std::string OPENCV_WINDOW = "Image window";
-
 class ThreadedImageWriter
 {
   image_transport::ImageTransport it_;
@@ -41,7 +39,7 @@ public:
         consumer[thread_num] = new Consumer<cv::Mat>(
                 buffer[thread_num].getBuffer(),
                 thread_fname, buffer[thread_num].getMutex(),
-                60.0f, 1280, 960, nh); 
+                50.0f, 1280, 960, nh); 
     }
     numframes = 0; 
     ROS_INFO_STREAM("Subscribing to topic " << topic << "...");
@@ -49,7 +47,6 @@ public:
     image_sub_ = it_.subscribe(topic, 1000, 
       &ThreadedImageWriter::imageCb, this);
 
-    //cv::namedWindow(OPENCV_WINDOW);
   }
 
   ~ThreadedImageWriter()
@@ -58,7 +55,6 @@ public:
           consumer[thread_num]->stop();
           delete consumer[thread_num];
       }
-    //cv::destroyWindow(OPENCV_WINDOW);
   }
 
   void imageCb(const sensor_msgs::ImageConstPtr& msg)
@@ -75,19 +71,13 @@ public:
       return;
     }
 
-    numframes++; // this happens to be a terrible detail that makes us effectively 1 index. 
+    numframes++; // this makes us effectively 1 index. 
     cv::Mat* img = new cv::Mat( cv_ptr->image ) ;
 
-    // FIXME
     if (!buffer[numframes % NUM_SPLITS].getBuffer()->pushBack(img)) {
-        boost::mutex::scoped_lock( *(buffer[0].getMutex()));
+        boost::mutex::scoped_lock( *(buffer[numframes % NUM_SPLITS].getMutex()));
         ROS_ERROR("Warning! Buffer full, overwriting data!");
     }
-
-    // Update GUI Window
-    //cv::imshow(OPENCV_WINDOW, cv_ptr->image);
-    //cv::waitKey(3);
-    
   }
 };
 
