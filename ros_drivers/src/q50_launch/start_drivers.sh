60e6284d0c8fd3a0d3e4bfcea37ce1529e91db40
Sameep Tandon <sameep@stanford.edu>
2014-03-26 15:23:15 -0700

    updating ros_home variable settings

--- ros_drivers/src/q50_launch/start_drivers.sh
+++ ros_drivers/src/q50_launch/start_drivers.sh
@@ -1,5 +1,3 @@
-export ROS_HOME=~/ros_data
-
 roslaunch q50_launch start_drivers.launch
 
 
--- ros_drivers/src/q50_launch/stop_data_collection.sh
+++ ros_drivers/src/q50_launch/stop_data_collection.sh
@@ -1,5 +1,3 @@
-export ROS_HOME=~/ros_data
-
 roslaunch novatel_serial_driver stop_triggering.launch &
 sleep 2 # wait for ROS buffers to clean up 
 rosnode kill /fwd_left_writer /fwd_right_writer /rosbag_record_gps /rosbag_record_odb /rosbag_record_radar /tcpdump/velodyne_node
