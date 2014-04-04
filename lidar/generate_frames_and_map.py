2256d0cd2e7612a90ff7d1a809f96b4ddec5958b
Jeff Kiske <jkiske09@gmail.com>
2014-04-04 13:46:19 -0700

    Updated Argparser and Generate to look for/create a config file

--- lidar/generate_frames_and_map.py
+++ lidar/generate_frames_and_map.py
@@ -5,8 +5,18 @@ LDR_COMMAND = 'build/LDRConverter --hdlcalibration 32db.xml --p '
 FF_COMMAND = 'python FrameFinder.py '
 GPS_COMMAND = 'python ../gps/BagToGPSOut.py '
 
+params = ''
+target_dir = ''
+
 def run_command(args):
     (p, target_dir) = args
+    
+    param_file = target_dir + '/params.ini'
+    f = open(param_file, 'w')
+    f.write(params + '\n')
+    f.close()
+    print 'Wrote %s to %s' % (params, param_file)
+
     cmd = LDR_COMMAND + target_dir + '/' + p
     print cmd
     subprocess.call(cmd, shell=True)
@@ -26,12 +36,17 @@ def run_command(args):
     print cmd
     subprocess.call(cmd, shell=True)
 
-target_dir = sys.argv[1]
-files = os.listdir(target_dir)
-pcap_files = filter(lambda x: '.pcap' in x,  files)
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        print """Usage:
+            generate_frames_and_map.py <folder> <which parameter file to load>"""
+        exit(-1)
 
-pool = multiprocessing.Pool(processes=1)
-pool.map(run_command, 
-    zip(pcap_files, 
-      [target_dir]*len(pcap_files))) 
+    target_dir = sys.argv[1]
+    files = os.listdir(target_dir)
+    pcap_files = filter(lambda x: '.pcap' in x,  files)
 
+    params = sys.argv[2]
+    pool = multiprocessing.Pool(processes=1)
+    pool.map(run_command,
+        zip(pcap_files, [target_dir]*len(pcap_files))) 
--- process/ArgParser.py
+++ process/ArgParser.py
@@ -1,8 +1,10 @@
+from Q50_config import LoadParameters
+
 def parse_args(folder, video_file):
     """Folder is the top level folder.
        video_file is the name of the camera split to use
        ex: parse_args('data/280S', '280S_a2.avi')
-       returns a dictionary with keys: ('map', 'frames', 'gps', 'video') 
+       returns a dictionary with keys: ('map', 'frames', 'radar' 'gps', 'video', 'params') 
        corresponding to file names.
     """
 
@@ -13,12 +15,15 @@ def parse_args(folder, video_file):
     radar_folder = basename + '_rdr/'
     gps_file = basename + '_gps.out'
     video_file = folder + '/' + video_file
+    param_file = folder + '/params.ini'
+    params = open(param_file, 'r').readline().rstrip()
 
     return {'map': map_file,
             'frames': lidar_folder,
             'radar': radar_folder,
             'gps': gps_file,
-            'video': video_file}
+            'video': video_file,
+            'params': LoadParameters(params)}
 
 
 # print parse_args('data/280S', '280S_a2.avi')
