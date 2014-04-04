251a254cede0a209c3618a731242889450c4d694
Sameep Tandon <sameep@stanford.edu>
2014-04-03 19:11:04 -0700

    fix

--- process/Q50_config.py
+++ process/Q50_config.py
@@ -1,5 +1,5 @@
 def LoadParameters(name):
-    if name == 'q50_4_2_14_params':
+    if name == 'q50_4_3_14_params':
         from parameters.q50_4_3_14_params import GetQ50Params
         return GetQ50Params()
     elif name == 'q50_3_7_14_params':
