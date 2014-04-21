4b4b174ad597e86cea008f233d852e1dc2bc3ced
Ziang Xie <zxie@cs.stanford.edu>
2014-04-21 14:25:06 -0700

    start cluster setup

---
+++ cloudproc/pipeline/fabfile.py
@@ -0,0 +1,18 @@
+import os
+from os.path import join as pjoin
+from fabric.api import env, run, task
+from pipeline_config import CLUSTER_DSET_DIR, FABRIC_PASS_FILE, CLUSTER_HOSTS,\
+        LDR_DIR
+
+# PARAM
+env.hosts = CLUSTER_HOSTS
+env.password = open(FABRIC_PASS_FILE, 'r').read().strip()
+env.colorize_errors = True
+
+@task
+def mkdirs():
+    run('mkdir -p %s' % CLUSTER_DSET_DIR)
+
+
+if __name__ == '__main__':
+    pass
