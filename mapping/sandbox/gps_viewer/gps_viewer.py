69a46caeadd5cd2d1f7ee1ccd3c4aaab4284f789
Ziang Xie <zxie@cs.stanford.edu>
2014-04-23 13:55:13 -0700

    visualizing tracks across days works

--- mapping/sandbox/gps_viewer/gps_viewer.py
+++ mapping/sandbox/gps_viewer/gps_viewer.py
@@ -25,8 +25,9 @@ if __name__ == '__main__':
     API_KEY = open('api_key.txt', 'r').read().strip()
 
     SCAIL_Q50_DATA_DIR = '/scail/group/deeplearning/driving_data/q50_data'
-    route_segment_split_gps_files = defaultdict(lambda: defaultdict(dict))
-    routes = [d for d in os.listdir(SCAIL_Q50_DATA_DIR) if os.path.isdir(d)]
+    route_segment_split_gps = defaultdict(lambda: defaultdict(dict))
+    routes = [d for d in os.listdir(SCAIL_Q50_DATA_DIR)]
+    print routes
     for route in routes:
         route_dir = os.path.join(SCAIL_Q50_DATA_DIR, route)
         gps_out_files = [f for f in os.listdir(route_dir) if f.endswith('gps.out')]
@@ -34,35 +35,36 @@ if __name__ == '__main__':
         for gps_out_file in gps_out_files:
             parts = gps_out_file.split('_')
             split = [part for part in parts if (len(part) == 1 and part.isalpha())][0]
-            segment = parts[0:parts.index(split)]
-            route_segment_split_gps_files[route][segment][split] =\
+            segment = '_'.join(parts[0:parts.index(split)])
+            route_segment_split_gps[route][segment][split] =\
                 {'gps_file': os.path.join(route_dir, gps_out_file)}
 
-    gps_files = list()
-    gps_files.append(GPS_FILE)
-    gps_files.append('%s/280N_a2/280N_a_gps.out' % DATA_DIR)
-    gps_files.append('%s/sandhill_b2/sandhill_b_gps.out' % DATA_DIR)
-
-    track_names = ['17N_b', '280N_a', 'sandhill_b']
-
-    gps_tracks = dict()
     lat_max_all, lat_min_all = float('-inf'), float('inf')
     lon_max_all, lon_min_all = float('-inf'), float('inf')
 
-    for k in range(len(gps_files)):
-        gps_file = gps_files[k]
-        track_name = track_names[k]
+    for route in route_segment_split_gps:
+        print route
+        segment_split_gps = route_segment_split_gps[route]
+        for segment in segment_split_gps:
+            print '\t' + segment
+            split_gps = segment_split_gps[segment]
+            for split in split_gps:
+                gps_file = split_gps[split]['gps_file']
+                print '\t\t' + split + ': ' + gps_file
 
-        lat, lon = read_lat_lon(gps_file)
-        lat = downsample_points(lat, 1000)
-        lon = downsample_points(lon, 1000)
+                lat, lon = read_lat_lon(gps_file)
+                lat = downsample_points(lat, 1000)
+                lon = downsample_points(lon, 1000)
+                if len(lat) == 0:
+                    continue
 
-        gps_tracks[track_name] = {'lat': lat.tolist(), 'lon': lon.tolist()}
+                split_gps[split]['lat'] = lat.tolist()
+                split_gps[split]['lon'] = lon.tolist()
 
-        lat_max, lat_min = max(lat), min(lat)
-        lon_max, lon_min = max(lon), min(lon)
-        lat_max_all, lat_min_all = max(lat_max, lat_max_all), min(lat_min, lat_min_all)
-        lon_max_all, lon_min_all = max(lon_max, lon_max_all), min(lon_min, lon_min_all)
+                lat_max, lat_min = max(lat), min(lat)
+                lon_max, lon_min = max(lon), min(lon)
+                lat_max_all, lat_min_all = max(lat_max, lat_max_all), min(lat_min, lat_min_all)
+                lon_max_all, lon_min_all = max(lon_max, lon_max_all), min(lon_min, lon_min_all)
 
     lat_center = (lat_max_all + lat_min_all) / 2
     lon_center = (lon_max_all + lon_min_all) / 2
@@ -71,4 +73,4 @@ if __name__ == '__main__':
     output = template.format(api_key=API_KEY, lat_center=lat_center, lon_center=lon_center)
     open('viewer.html', 'w').write(output)
 
-    json.dump(gps_tracks, open('gps_tracks.json', 'w'))
+    json.dump(route_segment_split_gps, open('gps_tracks.json', 'w'))
--- mapping/sandbox/gps_viewer/viewer.js
+++ mapping/sandbox/gps_viewer/viewer.js
@@ -2,6 +2,7 @@
 
 var map;
 var numTracks = 0;
+var numRoutes = 0;
 // http://en.wikipedia.org/wiki/Web_colors
 var strokeColors = [
     '#ff0000', // red
@@ -10,11 +11,10 @@ var strokeColors = [
     '#800000', // maroon
     '#000000', // black
     '#800080', // purple
-    '#ffff00', // yellow
+    '#ff00ff', // fuchsia
     '#808000', // olive
     '#ffffff', // white
     '#00ffff', // aqua
-
 ];
 var paths = [];
 var tracksActive = [];
@@ -31,23 +31,35 @@ function initializeMap() {
 // Load GPS
 
 function loadGPSTracks(f) {
-    $.getJSON(f, function(data) {
-        $.each(data, function(key, val)
-        {
-            console.log(key);
-            console.log(val['lat'].length)
-            var lat = val['lat'];
-            var lon = val['lon'];
-            drawGPSTrack(lat, lon);
-            addTrackPanel(key);
-            tracksActive.push(true);
-            numTracks++;
+    $.getJSON(f, function(route_segment_split_gps) {
+        $.each(route_segment_split_gps, function(route, segment_split_gps) {
+            routeDiv = $('<div class="routeDiv">' + route + '</div>')
+            console.log(route);
+            $.each(segment_split_gps, function (segment, split_gps) {
+                console.log('\t' + segment);
+                segmentDiv = $('<div class="segmentDiv">' + segment + '</div>')
+                $.each(split_gps, function(split, gps) {
+                    if (!('lat' in gps))
+                        return;
+                    console.log('\t\t' + split + ': ' + gps['lat'].length);
+                    var lat = gps['lat'];
+                    var lon = gps['lon'];
+                    drawGPSTrack(lat, lon);
+                    segmentDiv.append('<div class="trackPanel"><input type="checkbox" checked="true" onclick="toggleTrack(' + numTracks + ')" /><span style="color:' + strokeColors[numRoutes % strokeColors.length] + '">' + split + '</span></div>');
+                    tracksActive.push(true);
+                    numTracks++;
+                });
+                routeDiv.append(segmentDiv);
+            });
+            addTrackPanel(routeDiv);
+            numRoutes++;
         });
     });
 }
 
-function addTrackPanel(name) {
-    $('#trackPanels').append('<div class="trackPanel"><input type="checkbox" checked="true" onclick="toggleTrack(' + numTracks + ')" /><span style="color:' + strokeColors[numTracks] + '">' + name + '</span></div>');
+function addTrackPanel(routeDiv) {
+    $('#trackPanels').append(routeDiv);
+    //$('#trackPanels').append('<div class="trackPanel"><input type="checkbox" checked="true" onclick="toggleTrack(' + numTracks + ')" /><span style="color:' + strokeColors[numRoutes % strokeColors.length] + '">' + name + '</span></div>');
 }
 
 function toggleTrack(ind) {
@@ -70,7 +82,7 @@ function drawGPSTrack(lat, lon) {
     var path = new google.maps.Polyline({
         path: coords,
         geodesic: true,
-        strokeColor: strokeColors[numTracks],
+        strokeColor: strokeColors[numRoutes],
         strokeOpacity: 0.7,
         strokeWeight: 3
     });
