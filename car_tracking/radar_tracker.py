07678d5f2f8c14400f785d1ce1bca23bde7ecdf9
Hossein Karkeh Abadi <hosseink@stanford.edu>
2014-05-12 10:03:21 -0700

    Filling gaps (missed cars) using radar data.

--- car_tracking/radar_tracker.py
+++ car_tracking/radar_tracker.py
@@ -96,7 +96,7 @@ def set_annotations_ids_using_radar(annotations, rdr_map, args): # rect.classID:
 		for j in range(block_size):
 			idx = i + j;
 			frame_num = sc * idx;
-			if idx >=len(annotations) or frame_num > len(rdr_map):
+			if idx >=len(annotations) or frame_num >= len(rdr_map):
 				break;
 			annotation = annotations[idx];
 			radar_data = loadRDR(rdr_map[frame_num])[0]
@@ -139,7 +139,7 @@ def set_annotations_ids_using_radar(annotations, rdr_map, args): # rect.classID:
 		for j in range(block_size):
 			idx = i + j;
 			frame_num = sc * idx;
-			if idx >= len(annotations) or frame_num > len(rdr_map):
+			if idx >= len(annotations) or frame_num >= len(rdr_map):
 				break;
 			annotation = annotations[idx]
 			radar_data = loadRDR(rdr_map[frame_num])[0]
@@ -200,8 +200,136 @@ def set_annotations_ids_using_radar(annotations, rdr_map, args): # rect.classID:
 							rect.score = radar_data[cn_idx][0];
 		i += block_size;
 	return annotations;
+def fill_gaps(annotations, rdr_map):
+	anno_size = len(annotations);
+	rdr_map_size = len(rdr_map);
+	ind = 0;
+	search_area = 10;
+	while(True):
+		frame_num = ind * 10;
+		if(ind >= anno_size or frame_num >=len(rdr_map)):
+			break;
+		annotation = annotations[ind];
+		radar_data = loadRDR(rdr_map[frame_num])[0];
+		if radar_data.shape[0] > 0:
+		    # Remove points that have a low radar cross-section
+		    mask = (radar_data[:, 5] > 0)
+		    # Remove points that are moving too fast (fixed objects)
+		    mask &= (radar_data[:, 6] > -20)
+		    radar_data = radar_data[mask]
+		if radar_data.shape[0] > 0:
+			radar_array = np.array(radar_data);
+			radar_ids = radar_data[:,7];
+			anno_ids = [rect.classID for rect in annotation.rects];
+			for r, rId in enumerate(radar_ids):
+				if rId not in anno_ids:
+					flagb = False;
+					flagf = False;
+					temp_arr = range(max(0,ind-search_area), ind);
+					temp_arr.reverse();
+					for jb in temp_arr:
+						back_anno = annotations[jb];
+						b_ids = [rect.classID for rect in back_anno.rects];
+						if rId in b_ids:
+							flagb = True;
+							break;
+					for jf in range(ind, min(anno_size, ind + search_area)):
+						forward_anno = annotations[jf];
+						f_ids = [rect.classID for rect in forward_anno.rects];
+						if rId in f_ids:
+							flagf = True;
+							break;
+					if flagb and flagf:
+						back_rects = back_anno.rects;
+						forward_rects = forward_anno.rects;
+						for rect in back_rects:
+							if rect.classID == rId:
+								brect = rect;
+								break;
+							
+						for rect in forward_rects:
+							if rect.classID == rId:
+								frect = rect;
+								break;
+						ratio = (1.0*(ind-jb))/(jf-jb);
+						b_x1 = brect.x1; f_x1 = frect.x1;
+						b_x2 = brect.x2; f_x2 = frect.x2;
+						b_y1 = brect.y1; f_y1 = frect.y1;
+						b_y2 = brect.y2; f_y2 = frect.y2;
+						n_x1 = b_x1*(1-ratio) + f_x1*ratio;
+						n_x2 = b_x2*(1-ratio) + f_x2*ratio;
+						n_y1 = b_y1*(1-ratio) + f_y1*ratio;
+						n_y2 = b_y2*(1-ratio) + f_y2*ratio;
+						
+						new_rect = AnnoRect(int(n_x1), int(n_y1), int(n_x2), int(n_y2));
+						new_rect.classID = rId;
+						new_rect.score = 0;
+						annotations[ind].rects.append(new_rect)
+						print('Added');
+					elif flagb and jb > 0 and False:
+						back_rects = back_anno.rects;
+						for rect in back_rects:
+							if rect.classID == rId:
+								brect = rect;
+								break;
+						back_2_rects = annotations[jb-1].rects;
+						flagg = False;
+						for rect in back_2_rects:
+							if rect.classID == rId:
+								flagg = True;
+								b2rect = rect;
+								break;
+						if flagg:
+							k = ind - jb;
+							print k;
+							b_x1 = brect.x1; b2_x1 = b2rect.x1;
+							b_x2 = brect.x2; b2_x2 = b2rect.x2;
+							b_y1 = brect.y1; b2_y1 = b2rect.y1;
+							b_y2 = brect.y2; b2_y2 = b2rect.y2;
+							n_x1 = b_x1* (k+1) - b2_x1* k;
+							n_x2 = b_x2* (k+1) - b2_x2* k;
+							n_y1 = b_y1* (k+1) - b2_y1* k;
+							n_y2 = b_y2* (k+1) - b2_y2* k;
+							new_rect = AnnoRect(int(n_x1), int(n_y1), int(n_x2), int(n_y2));
+							new_rect.classID = rId;
+							new_rect.score = 0;
+							annotations[ind].rects.append(new_rect)
+							print('Added');
 
-def show_3D(annotations,rdr_map, args, save_video = False, with_options=False):
+					elif flagf and jf < anno_size - 2 and False:
+						forward_rects = forward_anno.rects;
+						for rect in forward_rects:
+							if rect.classID == rId:
+								frect = rect;
+								break;
+						forward_2_rects = annotations[jf+1].rects;
+						flagg = False;
+						for rect in forward_2_rects:
+							if rect.classID == rId:
+								flagg = True;
+								f2rect = rect;
+								break;
+						if flagg:
+							k = jf - ind ;
+							f_x1 = frect.x1; f2_x1 = f2rect.x1;
+							f_x2 = frect.x2; f2_x2 = f2rect.x2;
+							f_y1 = frect.y1; f2_y1 = f2rect.y1;
+							f_y2 = frect.y2; f2_y2 = b2rect.y2;
+							n_x1 = f_x1* (k+1) - f2_x1* k;
+							n_x2 = f_x2* (k+1) - f2_x2* k;
+							n_y1 = f_y1* (k+1) - f2_y1* k;
+							n_y2 = f_y2* (k+1) - f2_y2* k;
+							new_rect = AnnoRect(int(n_x1), int(n_y1), int(n_x2), int(n_y2));
+							new_rect.classID = rId;
+	#						new_rect.score =
+							annotations[ind].rects.append(new_rect)
+							print('Added');
+		
+		ind += 1;
+		
+
+
+def show_3D(annotations,rdr_map, args, save_video = False, with_options=False, with_radar=False):
 	if (save_video):
 		writer = cv2.VideoWriter('edited_sequence_test3D.avi', cv.CV_FOURCC('X','V', 'I', 'D'),
 			    20.0, (1280,960) )	
@@ -310,26 +438,48 @@ def show_3D(annotations,rdr_map, args, save_video = False, with_options=False):
 				#		cv2.line(I, tuple(bfr), tuple(bfl), (255,255,0))
 						if not flag0:
 							cv2.line(I, tuple(bfl), tuple(bbl), (255,255,0))
-						cv2.line(I, tuple(bbl), tuple(bbr), (255,0,0))
+						if rect.score==0:
+							cv2.line(I, tuple(bbl), tuple(bbr), (0,255,0))
+						else:
+							cv2.line(I, tuple(bbl), tuple(bbr), (255,0,0))
 						if flag0: 
 							cv2.line(I, tuple(bbr), tuple(bfr), (255,255,0))
-					
+							
 						cv2.line(I, tuple(tfr), tuple(tfl), (255,255,0),1)
 						cv2.line(I, tuple(tfl), tuple(tbl), (255,255,0),1)
-						cv2.line(I, tuple(tbl), tuple(tbr), (255,0,0),2)
+						if rect.score==0:
+							cv2.line(I, tuple(tbl), tuple(tbr), (0,255,0),2)
+						else:
+							cv2.line(I, tuple(tbl), tuple(tbr), (255,0,0),2)
 						cv2.line(I, tuple(tbr), tuple(tfr), (255,255,0),1)
 						if flag0:
 							cv2.line(I, tuple(tfr), tuple(bfr), (255,255,0))
 						if not flag0:
 							cv2.line(I, tuple(tfl), tuple(bfl), (255,255,0))
-						cv2.line(I, tuple(tbl), tuple(bbl), (255,0,0),2)
-						cv2.line(I, tuple(tbr), tuple(bbr), (255,0,0),2)
+						if rect.score==0:
+							cv2.line(I, tuple(tbl), tuple(bbl), (0,255,0),2)
+						else:
+							cv2.line(I, tuple(tbl), tuple(bbl), (255,0,0),2)
+
+						if rect.score==0:
+							cv2.line(I, tuple(tbr), tuple(bbr), (0,255,0),2)
+						else:
+							cv2.line(I, tuple(tbr), tuple(bbr), (255,0,0),2)
 						
-						cv2.putText(I, str(rect.score).split('.')[0], tuple(tbr),
+						cv2.putText(I, str(rect.classID), tuple(tbr),
 						    cv2.FONT_HERSHEY_SIMPLEX, .5, (0,0,255), thickness=1)
 	#	    mmap = RadarTrackerMapping(right_proj[:,8:10], np.array(bottom_right_pnt));
-		    
-		
+		    if with_radar:
+			for j in xrange(front_right_proj.shape[0]):
+				fr = front_right_proj[j, 8:10].astype(np.int32)
+				fl = front_left_proj[j, 8:10].astype(np.int32)
+				bl = left_proj[j, 8:10].astype(np.int32)
+				br = right_proj[j, 8:10].astype(np.int32)
+
+				cv2.line(I, tuple(fr), tuple(fl), (255,255,0))
+				cv2.line(I, tuple(fl), tuple(bl), (255,255,0))
+				cv2.line(I, tuple(bl), tuple(br), (255,0,0))
+				cv2.line(I, tuple(br), tuple(fr), (255,255,0))
 		ind += 1;
 		cv2.imshow('img1',I);
 		if(save_video):
@@ -419,10 +569,10 @@ if __name__ == "__main__":
    	rdr_map = loadRDRCamMap(args['map'])
 	
 	set_annotations_ids_using_radar(annotations, rdr_map, args);   
-	
+#	fill_gaps(annotations, rdr_map);
 	save_filename = filename.split('.')[0] + "_new.al";
 	saveXML(save_filename, annotations);
 #	compute_statistics(annotations, rdr_map);
-	show_3D(annotations, rdr_map,args, True);
-	print "Writing new annotations into a file..."
-	saveXML(filename.split('.')[0] + "_with_distance.al", annotations);
+#	show_3D(annotations, rdr_map,args, False, False, False);
+#	print "Writing new annotations into a file..."
+#	saveXML(filename.split('.')[0] + "_with_distance.al", annotations);
