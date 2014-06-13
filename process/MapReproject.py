d81babf24cc4667d7180482e15315dd4db71ecb5
Sameep Tandon <sameep@stanford.edu>
2014-06-13 11:36:03 -0700

    update mapreproject to conform to autopep8

--- process/MapReproject.py
+++ process/MapReproject.py
@@ -1,5 +1,6 @@
 from Q50_config import *
-import sys, os
+import sys
+import os
 from GPSReader import *
 from GPSTransforms import *
 from VideoReader import *
@@ -14,29 +15,32 @@ from LidarTransforms import interp_transforms, transform_points_by_times
 
 WINDOW = 2.0 * 1e6
 
-def cloudToPixels(cam, pts_wrt_cam): 
+
+def cloudToPixels(cam, pts_wrt_cam):
 
     width = 4
-    (pix, J)  = cv2.projectPoints(pts_wrt_cam.transpose(), np.array([0.0,0.0,0.0]), np.array([0.0,0.0,0.0]), cam['KK'], cam['distort'])
+    (pix, J) = cv2.projectPoints(pts_wrt_cam.transpose(), np.array(
+        [0.0, 0.0, 0.0]), np.array([0.0, 0.0, 0.0]), cam['KK'], cam['distort'])
 
     pix = pix.transpose()
     pix = np.around(pix[:, 0, :])
     pix = pix.astype(np.int32)
-    mask = np.logical_and(True, pix[0,:] > 0 + width/2)
-    mask = np.logical_and(mask, pix[1,:] > 0 + width/2)
-    mask = np.logical_and(mask, pix[1,:] < cam['height'] - 1 - width/2)
-    mask = np.logical_and(mask, pix[0,:] < cam['width'] - 1 - width/2)
-    mask = np.logical_and(mask, pts_wrt_cam[2,:] > 0)
+    mask = np.logical_and(True, pix[0, :] > 0 + width/2)
+    mask = np.logical_and(mask, pix[1, :] > 0 + width/2)
+    mask = np.logical_and(mask, pix[1, :] < cam['height'] - 1 - width/2)
+    mask = np.logical_and(mask, pix[0, :] < cam['width'] - 1 - width/2)
+    mask = np.logical_and(mask, pts_wrt_cam[2, :] > 0)
     dist_sqr = np.sum( pts_wrt_cam[0:3, :] ** 2, axis = 0)
     mask = np.logical_and(mask, dist_sqr > 3)
 
     return (pix, mask)
 
+
 def lidarPtsToPixels(map_data, imu_transforms_t, T_from_i_to_l, cam):
     # Transform points back to imu_t
-    pts_wrt_imu_0 = array(map_data[:,0:3]).transpose()
-    pts_wrt_imu_0 = np.vstack((pts_wrt_imu_0, 
-        np.ones((1,pts_wrt_imu_0.shape[1]))))
+    pts_wrt_imu_0 = array(map_data[:, 0:3]).transpose()
+    pts_wrt_imu_0 = np.vstack((pts_wrt_imu_0,
+                               np.ones((1, pts_wrt_imu_0.shape[1]))))
     pts_wrt_imu_t = np.dot(np.linalg.inv(imu_transforms_t), pts_wrt_imu_0)
     #pts_wrt_imu_t = pts_wrt_imu_0
 
@@ -44,20 +48,20 @@ def lidarPtsToPixels(map_data, imu_transforms_t, T_from_i_to_l, cam):
     pts_wrt_lidar_t = np.dot(T_from_i_to_l, pts_wrt_imu_t)
 
     # transform points from lidar_t to camera_t
-    pts_wrt_camera_t = pts_wrt_lidar_t.transpose()[:, 0:3] + cam['displacement_from_l_to_c_in_lidar_frame']
+    pts_wrt_camera_t = pts_wrt_lidar_t.transpose()[
+        :, 0:3] + cam['displacement_from_l_to_c_in_lidar_frame']
     pts_wrt_camera_t = dot(R_to_c_from_l(cam),
-            pts_wrt_camera_t.transpose())
+                           pts_wrt_camera_t.transpose())
 
     pts_wrt_camera_t = np.vstack((pts_wrt_camera_t,
-        np.ones((1, pts_wrt_camera_t.shape[1]))))
+                                  np.ones((1, pts_wrt_camera_t.shape[1]))))
     pts_wrt_camera_t = dot(cam['E'], pts_wrt_camera_t)
-    pts_wrt_camera_t = pts_wrt_camera_t[0:3,:]
+    pts_wrt_camera_t = pts_wrt_camera_t[0:3, :]
 
     # reproject camera_t points in camera frame
     (pix, mask) = cloudToPixels(cam, pts_wrt_camera_t)
 
     return (pix, mask)
-
 
 
 if __name__ == '__main__':
@@ -66,8 +70,8 @@ if __name__ == '__main__':
     video_file = args['video']
     video_reader = VideoReader(video_file)
     params = args['params']
-    cam = params['cam'][cam_num-1]
-    
+    cam = params['cam'][cam_num - 1]
+
     all_data = np.load(sys.argv[3])
     map_data = all_data['data']
     map_t = all_data['t']
@@ -89,20 +93,21 @@ if __name__ == '__main__':
         (success, I) = video_reader.getNextFrame()
         fnum = video_reader.framenum
         if cam_num > 2:
-            fnum *= 2 
+            fnum *= 2
         #t = utc_from_gps_log(gps_data_mark2[fnum, :])
         t = gps_times_mark2[fnum]
         fnum_mark1 = bisect.bisect(gps_times_mark1, t) - 1
 
-        mask_window = (map_t < t + WINDOW) & (map_t > t - WINDOW);
+        mask_window = (map_t < t + WINDOW) & (map_t > t - WINDOW)
         map_data_copy = array(map_data[mask_window, :]);
-        (pix, mask) = lidarPtsToPixels(map_data_copy, imu_transforms_mark1[fnum_mark1, :, :], T_from_i_to_l, cam)
+        (pix, mask) = lidarPtsToPixels(map_data_copy,
+                imu_transforms_mark1[fnum_mark1, :,:], T_from_i_to_l, cam)
 
         intensity = map_data_copy[mask, 3]
         heat_colors = heatColorMapFast(intensity, 0, 100)
         for p in range(4):
-            I[pix[1,mask]+p, pix[0,mask], :] = heat_colors[0,:,:]
-            I[pix[1,mask], pix[0,mask]+p, :] = heat_colors[0,:,:]
+            I[pix[1, mask]+p, pix[0, mask], :] = heat_colors[0,:,:]
+            I[pix[1, mask], pix[0, mask]+p, :] = heat_colors[0,:,:]
 
         cv2.imshow('vid', cv2.pyrDown(I))
         cv2.waitKey(50)
