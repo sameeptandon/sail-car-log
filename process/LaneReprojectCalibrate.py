870a7fb432c7f5fe1fda51d3e9e9bf47a1c9f2f7
Tao Wang <twangcat@robo.stanford.edu>
2014-07-31 18:20:56 -0700

    using lanes to do camera to imu calibration

--- process/LaneReprojectCalibrate.py
+++ process/LaneReprojectCalibrate.py
@@ -26,6 +26,41 @@ port = 3000 + int(random.random()*10000)
 def ParametersToString(rx,ry,rz,crx,cry,crz):
     return "%f,%f,%f,%f,%f,%f\n" % (rx,ry,rz,crx,cry,crz)
 
+
+
+
+def zDistances(self, distances, global_frame, starting_point, meters_per_point, points_fwd):
+    output = []
+    point_num = 1
+    dist = 0
+    for pt in xrange(points_fwd):
+      dist = pt * meters_per_point+starting_point
+      output.append((np.abs(distances-dist)).argmin()+global_frame)
+
+
+def lanePos(map_pos, imu_transforms_t, cam, T_from_i_to_l):
+    # load nearby map frames
+    pts_wrt_imu_0 = array(map_pos).transpose()
+    pts_wrt_imu_0 = np.vstack((pts_wrt_imu_0, np.ones((1,pts_wrt_imu_0.shape[1]))))
+    # transform points from imu_0 to imu_t
+    pts_wrt_imu_t = np.dot( np.linalg.inv(imu_transforms_t), pts_wrt_imu_0)
+    # transform points from imu_t to lidar_t
+    pts_wrt_lidar_t = np.dot(T_from_i_to_l, pts_wrt_imu_t);
+    # transform points from lidar_t to camera_t
+    pts_wrt_camera_t = pts_wrt_lidar_t.transpose()[:, 0:3] + cam['displacement_from_l_to_c_in_lidar_frame']
+    pts_wrt_camera_t = np.dot(cR, np.dot(R_to_c_from_l_old(0),
+            pts_wrt_camera_t.transpose()))    
+    pts_wrt_camera_t = np.vstack((pts_wrt_camera_t,
+        np.ones((1,pts_wrt_camera_t.shape[1]))))
+    pts_wrt_camera_t = dot(cam['E'], pts_wrt_camera_t)
+    pts_wrt_camera_t = pts_wrt_camera_t[0:3,:]
+    return pts_wrt_camera_t
+
+
+
+
+
+
 class RequestHandler(SocketServer.BaseRequestHandler):
     def handle(self):
         global rx, ry, rz, crx, cry, crz, R, cR, paramInit 
@@ -118,12 +153,18 @@ if __name__ == '__main__':
     video_reader = VideoReader(video_file)
     params = args['params'] 
     cam = params['cam'][cam_num-1]
-    gps_reader = GPSReader(args['gps_mark2'])
+    #gps_reader = GPSReader(args['gps_mark2'])
+    gps_filename= args['gps']
+    gps_reader = GPSReader(gps_filename)
+    prefix = gps_filename[0:-8]
     gps_data = gps_reader.getNumericData()
     lidar_loader = LDRLoader(args['frames'])
     imu_transforms = IMUTransforms(gps_data)
     gps_times = utc_from_gps_log_all(gps_data)
     
+    lane_filename = prefix+'_multilane_points_done.npz'
+    lanes = np.load(lane_filename)
+
     # parameter server
     thr = ThreadedServer(port)
     thr.setDaemon(True)
@@ -139,50 +180,59 @@ if __name__ == '__main__':
     (success, orig) = video_reader.getNextFrame()
     T_from_l_to_i = params['lidar']['T_from_l_to_i']
     T_from_i_to_l = np.linalg.inv(T_from_l_to_i)
+    fwd_range=100
+    colors = [(255,0,0),(0,255,0),(0,0,255),(255,255,0),(255,0,255),(0,255,255),(128,128,255),(128,255,128),(255,128,128),(128,128,0),(128,0,128),(0,128,128),(0,128,255),(0,255,128),(128,0,255),(128,255,0),(255,0,128),(255,128,0)]
     while True:
-        I = orig.copy()
-        fnum = video_reader.framenum
-        t = gps_times[fnum]
-        data, data_times = lidar_loader.loadLDRWindow(t-50000, 0.1)
+        
+      I = orig.copy()
+      fnum = video_reader.framenum
+      t = gps_times[fnum]
 
-        # Transform data into IMU frame at time t
-        pts = data[:, 0:3].transpose()
-        pts = np.vstack((pts, np.ones((1, pts.shape[1]))))
-        pts = np.dot(T_from_l_to_i, pts)
-
-        # Shift points according to timestamps instead of using transform of full sweep
-        #transform_points_by_times(pts, t_data, imu_transforms_mark1, gps_times_mark1)
-        transform_points_by_times(pts, data_times, imu_transforms, gps_times)
-
-        (pix, mask) = lidarPtsToPixels(pts, imu_transforms[fnum,:,:], T_from_i_to_l,cam); 
-        intensity = data[mask, 3]
-        heat_colors = heatColorMapFast(intensity, 0, 100)
+      ids = range(fnum, fnum+300)
+      for l in range(lanes['num_lanes']):
+        lane_key = 'lane'+str(l)
+        lane = lanes[lane_key]
+        # find the appropriate portion on the lane (close to the position of car, in front of camera, etc)
+        # find the closest point on the lane to the two end-points on the trajectory of car. ideally this should be done before-hand to increase efficiency.
+        dist_near = np.sum((lane-imu_transforms[ids[0],0:3,3])**2, axis=1) # find distances of lane to current 'near' position.
+        dist_far = np.sum((lane-imu_transforms[ids[-1],0:3,3])**2, axis=1) # find distances of lane to current 'far' position.
+        dist_mask = np.where(dist_near<=(fwd_range**2))[0]# only consider points to be valid within fwd_range from the 'near' position
+        if len(dist_mask)==0:
+          continue
+        nearid = np.argmin(dist_near[dist_mask]) # for those valid points, find the one closet to 'near' position.
+        farid = np.argmin(dist_far[dist_mask]) #and far position
+        lids = range(dist_mask[nearid], dist_mask[farid]+1) # convert back to global id and make it into a consecutive list. 
+        lane3d = lanePos(lane[lids,:], imu_transforms[fnum,:,:], cam,T_from_i_to_l) # lane markings in current camera frame
+        if np.all(lane3d[2,:]<=0):
+          continue
+        lane3d = lane3d[:,lane3d[2,:]>0] # make sure in front of camera
+        (pix, mask) = cloudToPixels(cam, lane3d)
         for p in range(4):
-            I[pix[1,mask]+p, pix[0,mask], :] = heat_colors[0,:,:]
-            I[pix[1,mask], pix[0,mask]+p, :] = heat_colors[0,:,:]
-            I[pix[1,mask]+p, pix[0,mask], :] = heat_colors[0,:,:]
-            I[pix[1,mask], pix[0,mask]+p, :] = heat_colors[0,:,:]
+            I[pix[1,mask]+p, pix[0,mask], :] = colors[(l)%18]
+            I[pix[1,mask], pix[0,mask]+p, :] = colors[(l)%18]
+            I[pix[1,mask]+p, pix[0,mask], :] = colors[(l)%18]
+            I[pix[1,mask], pix[0,mask]+p, :] = colors[(l)%18]
 
-        cv2.imshow('vid', I)
-        key = cv2.waitKey(10)
-        if key == -1:
-            continue
-        key = chr(key & 255)
-        if key == 'a':
-            cry += 0.001
-        elif key == 'd':
-            cry -= 0.001
-        elif key == 'w':
-            crx += 0.001
-        elif key == 's':
-            crx -= 0.001
-        elif key == '+':
-            crz += 0.001
-        elif key == '_' or key == '-':
-            crz -= 0.001
-        else:
-            continue
+      cv2.imshow('vid', I)
+      key = cv2.waitKey(10)
+      if key == -1:
+        continue
+      key = chr(key & 255)
+      if key == 'a':
+        cry += 0.001
+      elif key == 'd':
+        cry -= 0.001
+      elif key == 'w':
+        crx += 0.001
+      elif key == 's':
+        crx -= 0.001
+      elif key == '+':
+        crz += 0.001
+      elif key == '_' or key == '-':
+        crz -= 0.001
+      else:
+        continue
     
-        print (crx, cry, crz)
-        cR = euler_matrix(crx, cry, crz)[0:3,0:3]
-        sock.sendto('PARAMETER_UPDATE:'+str(port)+':'+ParametersToString(rx,ry,rz,crx,cry,crz), ('localhost', 2929))
+      print (crx, cry, crz)
+      cR = euler_matrix(crx, cry, crz)[0:3,0:3]
+      sock.sendto('PARAMETER_UPDATE:'+str(port)+':'+ParametersToString(rx,ry,rz,crx,cry,crz), ('localhost', 2929))
