37bf2eba9c34081d927059d8048095e5633ff330
Jeff Kiske <jkiske09@gmail.com>
2014-10-09 17:55:08 -0700

    LaneClassLabeler now shows a camera image

--- LaneClassLabeler/server.py
+++ LaneClassLabeler/server.py
@@ -1,4 +1,11 @@
+import cv2
+import glob
+import sys
+from ArgParser import parse_args
+from LaneClassLabeler import ImageLoader
+
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,4 +15,8 @@ def hello():
                            generated_src='static/test2.jpg')
 
 if __name__ == '__main__':
+    args = parse_args('../process/data/4-20-14-280/280N_a', '280N_a2.avi')
+    il = ImageLoader(args)
+    I = il.nextFrame(10)
+    cv2.imwrite('static/test.jpg', I)
     app.run(debug=True)
