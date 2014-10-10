6e48e54ed53cad7b266ce97a1eee636865862190
Jeff Kiske <jkiske09@gmail.com>
2014-10-09 17:03:58 -0700

    Shows two images next to each other

--- LaneClassLabeler/server.py
+++ LaneClassLabeler/server.py
@@ -3,7 +3,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return render_template('main.html', test='Hello')
+    return render_template('main.html', test='Hello',
+                           video_src="static/test.jpg",
+                           generated_src='static/test2.jpg')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
