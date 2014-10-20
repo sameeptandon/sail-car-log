4fca6fb43938e5de146162e9de3355fb285819fb
Jeff Kiske <jkiske09@gmail.com>
2014-10-20 15:59:39 -0700

    Added ability to find closest lanes

--- process/LidarLaneClassLabeler.py
+++ process/LidarLaneClassLabeler.py
@@ -1,4 +1,5 @@
 import numpy as np
+import glob
 import sys
 from ArgParser import parse_args
 from GPSTransforms import IMUTransforms
@@ -9,12 +10,13 @@ from scipy.interpolate import UnivariateSpline
 import matplotlib.pyplot as plt
 from LaneMarkingHelper import get_transforms, DataTree
 
-class PlaneFitter(object):
-    def __init__(self, args, ground_npz, multilane_npz):
+class LaneClassifier(object):
+    def __init__(self, args, multilane_npz):
         self.args = args
 
         lanes_npz = np.load(multilane_npz)
         self.num_lanes = lanes_npz['num_lanes']
+
         self.lanes = [None for x in range(self.num_lanes)]
 
         for i in xrange(self.num_lanes):
@@ -23,59 +25,60 @@ class PlaneFitter(object):
         self.imu, _, _ = get_transforms(args)
         self.pos = self.imu[:, :3, 3]
 
-    def findGroundPlane(self, pos_idx, radius=100.0, threshold=0.05, n_iter=10):
-        pts_idx = self.ground_tree.query_ball_point(self.pos[pos_idx, :], radius)
-        pts = self.ground[pts_idx, :]
+    def classifyNearbyLanes(self, pos_idx):
+        closest_lanes = np.ones((self.num_lanes, 2)) * -1
+        for i in xrange(self.num_lanes):
+            (d, idx) = self.lanes[i].tree.query(self.pos[pos_idx, :], distance_upper_bound=10)
+            closest_lanes[i] = np.array((d, idx))
 
-        # Remove points that are outside the lanes
-        xform = self.imu[pos_idx]
-        inv_xform = np.linalg.inv(xform)
+        arg_sort = np.argsort(closest_lanes[:, 0], axis=0)
+        print pos_idx, arg_sort[0], arg_sort[1], arg_sort[2], arg_sort[3]
 
-        _, left_idx = self.lanes_tree[0].query(self.pos[pos_idx], k=1)
-        _, right_idx = self.lanes_tree[-1].query(self.pos[pos_idx], k=1)
+        # pts_idx = self.ground_tree.query_ball_point(self.pos[pos_idx, :], radius)
+        # pts = self.ground[pts_idx, :]
 
-        lane_boundary = np.vstack((self.lanes[0][left_idx], self.lanes[-1][right_idx]))
-        lane_boundary = np.hstack((lane_boundary, np.ones((2, 1))))
-        lane_boundary = np.dot(lane_boundary, inv_xform.T)
+        # # Remove points that are outside the lanes
+        # xform = self.imu[pos_idx]
+        # inv_xform = np.linalg.inv(xform)
 
-        hom_pts = np.hstack((pts, np.ones((pts.shape[0], 1))))
-        rot_pts = np.dot(hom_pts, inv_xform.T)
+        # _, left_idx = self.lanes_tree[0].query(self.pos[pos_idx], k=1)
+        # _, right_idx = self.lanes_tree[-1].query(self.pos[pos_idx], k=1)
 
-        pt_mask = (rot_pts[:, 1] < lane_boundary[0, 1])  & \
-                  (rot_pts[:, 1] > lane_boundary[1, 1])
-        filt_pts = np.dot(rot_pts[pt_mask], xform.T)
+        # lane_boundary = np.vstack((self.lanes[0][left_idx], self.lanes[-1][right_idx]))
+        # lane_boundary = np.hstack((lane_boundary, np.ones((2, 1))))
+        # lane_boundary = np.dot(lane_boundary, inv_xform.T)
 
-        pts = filt_pts[:, :3]
+        # hom_pts = np.hstack((pts, np.ones((pts.shape[0], 1))))
+        # rot_pts = np.dot(hom_pts, inv_xform.T)
 
-        if pts.shape[0] < 10:
-            return (None,) * 3
+        # pt_mask = (rot_pts[:, 1] < lane_boundary[0, 1])  & \
+        #           (rot_pts[:, 1] > lane_boundary[1, 1])
+        # filt_pts = np.dot(rot_pts[pt_mask], xform.T)
 
-        # number of inliers, (idx0, idx1, idx2)
-        best_model = (0, np.array(0))
+        # pts = filt_pts[:, :3]
 
-        if end >= self.filt_ground.shape[0]:
-            print 'resizing'
-            self.filt_ground = np.concatenate((self.filt_ground,
-                                               np.empty(self.filt_ground.shape)))
-            print 'done'
+        # if pts.shape[0] < 10:
+        #     return (None,) * 3
 
-        self.filt_ground[start:end, :] = new_pts
-        self.filt_ground_idx = end
+        # # number of inliers, (idx0, idx1, idx2)
+        # best_model = (0, np.array(0))
 
-        return n, p0, err
+        # if end >= self.filt_ground.shape[0]:
+        #     print 'resizing'
+        #     self.filt_ground = np.concatenate((self.filt_ground,
+        #                                        np.empty(self.filt_ground.shape)))
+        #     print 'done'
+
+        # self.filt_ground[start:end, :] = new_pts
+        # self.filt_ground_idx = end
+
+        # return n, p0, err
 
 if __name__ == '__main__':
     args = parse_args(sys.argv[1], sys.argv[2])
-    plane_file = args['fullname'] + '_ground.npz'
-    lanes_file = sys.argv[1] + '/multilane_points.npz'
-    pf = PlaneFitter(args, plane_file, lanes_file)
+    lanes_file = glob.glob(sys.argv[1] + '/*done.npz')[0]
+    lc = LaneClassifier(args, lanes_file)
 
-    for i in xrange(0, pf.pos.shape[0], 25):
-    # for i in xrange(1000, 2000, 100):
-        if i % 1000 == 0:
-            print '%d/%d' % (i, pf.pos.shape[0])
-
-        n, p0, err = pf.findGroundPlane(i, radius=25)
-
-    pf.correctLanes()
-    pf.exportData(sys.argv[1] + '/multilane_points_planar.npz')
+    for i in xrange(0, lc.pos.shape[0], 25):
+        # print '%d/%d' % (i, lc.pos.shape[0])
+        lc.classifyNearbyLanes(i)
