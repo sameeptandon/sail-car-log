e83a6efa742a176fe6611b62d2643c2e73ce6d18
Jeff Kiske <jkiske09@gmail.com>
2014-10-29 13:43:47 -0700

    LidarLaneClassLabeler can export data

--- process/LidarLaneClassLabeler.py
+++ process/LidarLaneClassLabeler.py
@@ -24,42 +24,55 @@ class LaneClassifier(object):
 
         self.imu, _, _ = get_transforms(args)
         self.pos = self.imu[:, :3, 3]
-        self.nearest_pts = np.zeros((self.pos.shape, 2))
 
-    def classifyNearbyLanes(self, pos_idx):
-        # # Remove points that are outside the lanes
-        xform = self.imu[pos_idx]
-        inv_xform = np.linalg.inv(xform)
+    def classifyNearbyLanes(self, step = 10):
+        self.nearest_pts = np.zeros((self.pos.shape[0] / step + 1, 2, 3))
 
-        pt_left = None
-        pt_right = None
+        for pos_idx in xrange(0, self.pos.shape[0], step):
+            if pos_idx % 1000 == 0:
+                print '%d/%d' % (pos_idx, self.pos.shape[0])
 
-        for lane_num in xrange(self.num_lanes):
-            _, idx = self.lanes[lane_num].tree.query(self.pos[pos_idx], k=1)
-            hom_pt = np.hstack((self.lanes[lane_num].pts[idx, :], 1))
-            closest_pt_wrt_car = np.dot(hom_pt, inv_xform.T)
-            norm = np.linalg.norm(closest_pt_wrt_car)
+            xform = self.imu[pos_idx]
+            inv_xform = np.linalg.inv(xform)
 
-            if closest_pt_wrt_car[1] >= 0:
-                if pt_left == None or norm < pt_left[0]:
-                    pt_left = (norm, idx, lane_num)
-            if closest_pt_wrt_car[1] < 0:
-                if pt_right == None or norm < pt_right[0]:
-                    pt_right = (norm, idx, lane_num)
+            pt_left = None
+            pt_right = None
 
-        nearest_pts = np.array((self.lanes[pt_left[-1]].pts[pt_left[1]],
-                               (self.lanes[pt_right[-1]].pts[pt_right[1]])))
+            for lane_num in xrange(self.num_lanes):
+                _, idx = self.lanes[lane_num].tree.query(self.pos[pos_idx], k=1)
+                hom_pt = np.hstack((self.lanes[lane_num].pts[idx, :], 1))
+                closest_pt_wrt_car = np.dot(hom_pt, inv_xform.T)
+                norm = np.linalg.norm(closest_pt_wrt_car)
+
+                if closest_pt_wrt_car[1] >= 0:
+                    if pt_left == None or norm < pt_left[0]:
+                        pt_left = (norm, idx, lane_num)
+                if closest_pt_wrt_car[1] < 0:
+                    if pt_right == None or norm < pt_right[0]:
+                        pt_right = (norm, idx, lane_num)
+
+            nearest_pts = np.array((self.lanes[pt_left[-1]].pts[pt_left[1]],
+                                    (self.lanes[pt_right[-1]].pts[pt_right[1]])))
+            self.nearest_pts[pos_idx / step] = nearest_pts
+
 
         # print pos_idx
         # print nearest_pts
 
+    def exportData(self, file_name):
+        lanes = {}
+        lanes['num_lanes'] = 2
+
+        for num in xrange(2):
+            lane = self.nearest_pts[:, num, :]
+            lanes['lane' + str(num)] = lane
+
+        np.savez(file_name, **lanes)
+        print 'Saved', file_name
 
 if __name__ == '__main__':
     args = parse_args(sys.argv[1], sys.argv[2])
     lanes_file = glob.glob(sys.argv[1] + '/*done.npz')[0]
     lc = LaneClassifier(args, lanes_file)
-
-    for i in xrange(0, lc.pos.shape[0], 10):
-        if i % 1000 == 0:
-            print '%d/%d' % (i, lc.pos.shape[0])
-        lc.classifyNearbyLanes(i)
+    lc.classifyNearbyLanes(1)
+    lc.exportData(sys.argv[1] + '/test.npz')
