b08ff632b7a80ec05362e3a45257f4f38413fa9c
Jeff Kiske <jkiske09@gmail.com>
2014-11-12 17:21:11 -0800

    Added done lane to json

--- process/DriverseatPipeline.py
+++ process/DriverseatPipeline.py
@@ -168,3 +168,40 @@ if __name__ == '__main__':
                     json_zip.close()
 
             configurator.set('maps_json', True)
+        if configurator.config['lanes_done_json'] == False:
+            for remote_run in sorted(glob.glob(remote_folder + '*/')):
+                run = remote_run.split('/')[-2]
+                driverseat_run = driverseat_folder + run + '/'
+                done_folder = remote_folder.\
+                              replace('/data/', '/multilane_done/') + run + '/'
+                done_lane_files = glob.glob(done_folder + '*done*')
+                if len(done_lane_files) == 0:
+                    continue
+                done_lane_file = done_lane_files[0]
+
+                print driverseat_run
+
+                json_name = 'lanes_done.json'
+                json_lanes_name = driverseat_run + json_name
+                zip_lanes_name = json_lanes_name + '.zip'
+
+                if not os.path.isfile(json_lanes_name):
+                    print '\tExporting finished lanes JSON...'
+                    lanes = np.load(done_lane_file)
+                    num_lanes = lanes['num_lanes']
+                    lane_data = {}
+                    for i in xrange(num_lanes):
+                        lane_data[str(i)] = lanes['lane' + str(i)].tolist()
+
+                    with open(json_lanes_name, 'w') as json_gps_file:
+                        json.dump(lane_data, json_gps_file)
+
+                if not os.path.isfile(zip_lanes_name):
+                    print '\tZipping...'
+                    data = '\n'.join([x for x in open(json_lanes_name,
+                                                      'r').readlines()])
+                    json_zip = ZipFile(zip_lanes_name, 'w', ZIP_DEFLATED)
+                    json_zip.writestr(json_name, data)
+                    json_zip.close()
+
+            configurator.set('lanes_done_json', True)
