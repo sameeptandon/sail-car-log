5f0d6d28fa6034e7547e903ef2b9e13db3c4f3b8
Jeff Kiske <jkiske09@gmail.com>
2014-11-12 17:39:46 -0800

    Added planes and unfinished lanes

--- process/DriverseatPipeline.py
+++ process/DriverseatPipeline.py
@@ -59,6 +59,8 @@ if __name__ == '__main__':
         configurator.get('radar_json')
         configurator.get('gps_json')
         configurator.get('lanes_json')
+        configurator.get('planes_json')
+        configurator.get('lanes_done_json')
 
         print configurator.config
 
@@ -82,7 +84,7 @@ if __name__ == '__main__':
                 zip_map_name = json_map_name + '.zip'
 
                 if not os.path.isfile(json_map_name):
-                    print '\tExporting map JSON...'
+                    print '\tExporting' + json_name
                     data = np.load(npz_map_name)['data']
                     # Only includes x,y,z data for the maps
                     json.dump(data[:, :3].tolist(), open(json_map_name, 'w'))
@@ -112,8 +114,7 @@ if __name__ == '__main__':
                 zip_radar_name = json_radar_name + '.zip'
                 print driverseat_run
                 if not os.path.isfile(json_radar_name):
-                    print '\tExporting radar JSON...'
-
+                    print '\tExporting' + json_name
                     radar_times = [int(f.split('/')[-1][:-4]) for
                                    f in sorted(glob.glob(radar_folder + '*'))]
                     for i in xrange(gps_times_mk2.shape[0]):
@@ -149,7 +150,7 @@ if __name__ == '__main__':
                 zip_gps_name = json_gps_name + '.zip'
 
                 if not os.path.isfile(json_gps_name):
-                    print '\tExporting gps JSON...'
+                    print '\tExporting' + json_name
                     gps_data = []
                     for i in xrange(gps_times_mk2.shape[0]):
                         mk1_i = mk2_to_mk1(i, gps_times_mk1, gps_times_mk2)
@@ -167,7 +168,79 @@ if __name__ == '__main__':
                     json_zip.writestr(json_name, data)
                     json_zip.close()
 
-            configurator.set('maps_json', True)
+            configurator.set('gps_json', True)
+
+        if configurator.config['lanes_json'] == False:
+            for remote_run in sorted(glob.glob(remote_folder + '*/')):
+                run = remote_run.split('/')[-2]
+                driverseat_run = driverseat_folder + run + '/'
+                lane_file = remote_run + 'multilane_points_planar.npz'
+
+                if not os.path.isfile(lane_file):
+                    continue
+
+                print driverseat_run
+
+                json_name = 'lanes.json'
+                json_lanes_name = driverseat_run + json_name
+                zip_lanes_name = json_lanes_name + '.zip'
+
+                if not os.path.isfile(json_lanes_name):
+                    print '\tExporting' + json_name
+                    lanes = np.load(lane_file)
+                    num_lanes = lanes['num_lanes']
+                    lane_data = {}
+                    for i in xrange(num_lanes):
+                        lane_data[str(i)] = lanes['lane' + str(i)].tolist()
+
+                    plane_data = lanes['planes'].tolist()
+
+                    with open(json_lanes_name, 'w') as json_lanes_file:
+                        json.dump(lane_data, json_lanes_file)
+
+                if not os.path.isfile(zip_lanes_name):
+                    print '\tZipping...'
+                    data = '\n'.join([x for x in open(json_lanes_name,
+                                                      'r').readlines()])
+                    json_zip = ZipFile(zip_lanes_name, 'w', ZIP_DEFLATED)
+                    json_zip.writestr(json_name, data)
+                    json_zip.close()
+
+            configurator.set('lanes_json', True)
+
+        if configurator.config['planes_json'] == False:
+            for remote_run in sorted(glob.glob(remote_folder + '*/')):
+                run = remote_run.split('/')[-2]
+                driverseat_run = driverseat_folder + run + '/'
+                lane_file = remote_run + 'multilane_points_planar.npz'
+
+                if not os.path.isfile(lane_file):
+                    continue
+
+                print driverseat_run
+
+                json_name = 'planes.json'
+                json_planes_name = driverseat_run + json_name
+                zip_planes_name = json_planes_name + '.zip'
+
+                if not os.path.isfile(json_planes_name):
+                    print '\tExporting' + json_name
+                    lanes = np.load(lane_file)
+                    plane_data = lanes['planes'].tolist()
+
+                    with open(json_planes_name, 'w') as json_planes_file:
+                        json.dump(plane_data, json_planes_file)
+
+                if not os.path.isfile(zip_planes_name):
+                    print '\tZipping...'
+                    data = '\n'.join([x for x in open(json_planes_name,
+                                                      'r').readlines()])
+                    json_zip = ZipFile(zip_planes_name, 'w', ZIP_DEFLATED)
+                    json_zip.writestr(json_name, data)
+                    json_zip.close()
+
+            configurator.set('planes_json', True)
+
         if configurator.config['lanes_done_json'] == False:
             for remote_run in sorted(glob.glob(remote_folder + '*/')):
                 run = remote_run.split('/')[-2]
@@ -186,15 +259,15 @@ if __name__ == '__main__':
                 zip_lanes_name = json_lanes_name + '.zip'
 
                 if not os.path.isfile(json_lanes_name):
-                    print '\tExporting finished lanes JSON...'
+                    print '\tExporting' + json_name
                     lanes = np.load(done_lane_file)
                     num_lanes = lanes['num_lanes']
                     lane_data = {}
                     for i in xrange(num_lanes):
                         lane_data[str(i)] = lanes['lane' + str(i)].tolist()
 
-                    with open(json_lanes_name, 'w') as json_gps_file:
-                        json.dump(lane_data, json_gps_file)
+                    with open(json_lanes_name, 'w') as json_lanes_file:
+                        json.dump(lane_data, json_lanes_file)
 
                 if not os.path.isfile(zip_lanes_name):
                     print '\tZipping...'
